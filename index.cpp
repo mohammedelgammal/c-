c5bd1220a76c6cc7a9d42849fac9a7abb37e609a
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-07-22 13:09:06 +0300

    passing_array_to_function

--- index.cpp
+++ index.cpp
@@ -3,14 +3,15 @@
 
 using namespace std;
 
+void display_array(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << endl;
+}
+
 int main()
 {
-    int one[10] = {1, 2, 3};
-    int two[size(one)];
-
-    for (int index : one)
-        two[index] = one[index];
-
-    cout << two[2];
+    int arr[] = {1, 2, 4, 5, 6};
+    display_array(arr, size(arr));
     return 0;
 }
