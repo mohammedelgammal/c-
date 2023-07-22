48d1a4610761dc66c14ba5ffb8107494da5591db
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-07-22 14:24:53 +0300

    linear_search_through_array

--- index.cpp
+++ index.cpp
@@ -9,9 +9,23 @@ void display_array(int arr[], int size)
         cout << arr[i] << endl;
 }
 
+int search()
+{
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int target = 0;
+    int current = 0;
+    while (current < size(arr) - 1)
+    {
+        if (arr[current] == target)
+            return arr[current];
+        else
+            current++;
+    }
+    return -1;
+}
+
 int main()
 {
-    int arr[] = {1, 2, 4, 5, 6};
-    display_array(arr, size(arr));
+    cout << search();
     return 0;
 }
