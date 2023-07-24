3c5bb0358f26af262f5252a670860722c7670592
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-07-24 12:40:04 +0300

    dynamically_allocate_memory_vector_implementation

--- index.cpp
+++ index.cpp
@@ -1,33 +1,38 @@
 #include <iostream>
-#include <iomanip>
 
 using namespace std;
 
-void search()
-{
-    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    int temp_element = 0;
-    bool swapped = false;
-    do
-    {
-        swapped = false;
-        for (int index = 0; index < 8; index++)
-        {
-            if (arr[index] < arr[index + 1])
-            {
-                temp_element = arr[index + 1];
-                arr[index + 1] = arr[index];
-                arr[index] = temp_element;
-                swapped = true;
-            }
-        }
-    } while (swapped);
-    for (int el : arr)
-        cout << el << endl;
-}
-
 int main()
 {
-    search();
+    int maxCapacity = 5;
+    int *numbers = new int[maxCapacity];
+    int entries = 0;
+    while (true)
+    {
+        cout << "Number: " << endl;
+        cin >> numbers[entries];
+        if (cin.fail())
+            break;
+        entries++;
+        if (entries > maxCapacity)
+        {
+            maxCapacity *= 2;
+            int *temp = new int[maxCapacity];
+            for (int i = 0; i < entries; i++)
+            {
+                temp[i] = numbers[i];
+            }
+            delete[] numbers;
+            numbers = temp;
+            temp = nullptr;
+        }
+    }
+    for (int i = 0; i < entries; i++)
+    {
+        cout << numbers[i] << endl;
+    }
+    delete[] numbers;
+    numbers = nullptr;
+
     return 0;
 }
