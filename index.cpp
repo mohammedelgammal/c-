81eec0b84175faa71c60820f0f726b9ec1c11b4a
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-07-24 23:13:10 +0300

    extracting_first_name_&_last_name_using_string_methods

--- index.cpp
+++ index.cpp
@@ -1,38 +1,16 @@
 #include <iostream>
+#include <memory>
 
 using namespace std;
 
 int main()
 {
-    int maxCapacity = 5;
-    int *numbers = new int[maxCapacity];
-    int entries = 0;
-    while (true)
-    {
-        cout << "Number: " << endl;
-        cin >> numbers[entries];
-        if (cin.fail())
-            break;
-        entries++;
-        if (entries > maxCapacity)
-        {
-            maxCapacity *= 2;
-            int *temp = new int[maxCapacity];
-            for (int i = 0; i < entries; i++)
-            {
-                temp[i] = numbers[i];
-            }
-            delete[] numbers;
-            numbers = temp;
-            temp = nullptr;
-        }
-    }
-    for (int i = 0; i < entries; i++)
-    {
-        cout << numbers[i] << endl;
-    }
-    delete[] numbers;
-    numbers = nullptr;
-
+    string word = "mohammed elgammal";
+    int firstSpaceIndex = word.find(' ');
+    int lastSpaceIndex = word.rfind(' ') + 1;
+    string firstName = word.substr(0, firstSpaceIndex);
+    string lastName = word.substr(lastSpaceIndex);
+    cout << firstName << endl;
+    cout << lastName << endl;
     return 0;
 }
