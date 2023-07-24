ed6ad91daa99abd31c11855a9fdbda7455c36221
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-07-25 01:22:36 +0300

    adding_date_to_movie_structor

--- index.cpp
+++ index.cpp
@@ -2,23 +2,30 @@
 
 using namespace std;
 
+struct Date
+{
+    short year = 1800,
+        month = 1,
+        day = 1;
+};
+
 struct Movie
 {
     string title;
-    int releaseYear = 0;
+    Date releaseDate;
     bool isPopular = false;
 };
 
 int main()
 {
-    Movie terminatorOne = {"Terminator", 1985, true},
-          terminatorTwo = {"Terminator 2", 1991, false},
-          terminatorThree = {"Terminator 3", 1995, true};
+    Movie terminatorOne = {"Terminator", {1983, 9, 4}, true},
+          terminatorTwo = {"Terminator 2", {1991, 10, 3}, false},
+          terminatorThree = {"Terminator 3", {1995, 1, 20}, true};
     vector<Movie> terminator = {terminatorOne, terminatorTwo};
     terminator.push_back(terminatorThree);
     for (Movie &movie : terminator)
     {
-        cout << movie.title << ", " << movie.releaseYear << endl;
+        cout << movie.title << ", " << movie.releaseDate.day << ", " << movie.releaseDate.month << ", " << movie.releaseDate.year << endl;
     }
     return 0;
 }
