088e960cc8dc28f0d7ff768f12ab778bc622c186
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-07-25 01:50:52 +0300

    built_complete_user_model_structure_applying_destructoring

--- index.cpp
+++ index.cpp
@@ -4,28 +4,39 @@ using namespace std;
 
 struct Date
 {
-    short year = 1800,
-        month = 1,
-        day = 1;
+    short day = 1,
+          month = 1,
+          year = 1800;
 };
-
-struct Movie
+struct Address
 {
-    string title;
-    Date releaseDate;
-    bool isPopular = false;
+    string street,
+        city;
+    int zipCode = 0;
+};
+struct Customer
+{
+    long long ID = 0;
+    string name;
+    Date dob;
+    Address address;
+    bool isRegistered = false;
 };
 
 int main()
 {
-    Movie terminatorOne = {"Terminator", {1983, 9, 4}, true},
-          terminatorTwo = {"Terminator 2", {1991, 10, 3}, false},
-          terminatorThree = {"Terminator 3", {1995, 1, 20}, true};
-    vector<Movie> terminator = {terminatorOne, terminatorTwo};
-    terminator.push_back(terminatorThree);
-    for (Movie &movie : terminator)
+    Customer customerOne = {75384, "Mohammed", {5, 12, 1987}, {"King Charl's st.", "Leeds", 8412}, true},
+             customerTwo = {69530, "Amr", {4, 9, 1991}, {"Salah Salem st.", "Cairo", 99211}, false},
+             customerThree = {85032, "Mahmoud", {21, 3, 1998}, {"67 Main road east", "Queen's Land", 22180}, true};
+    vector<Customer> customers = {customerOne, customerTwo};
+    customers.push_back(customerThree);
+    for (Customer &customer : customers)
     {
-        cout << movie.title << ", " << movie.releaseDate.day << ", " << movie.releaseDate.month << ", " << movie.releaseDate.year << endl;
+        auto [ID, name, dob, address, isRegistered] = customer;
+        cout << boolalpha << endl
+             << "User: " << name << ", id(" << ID << ")" << endl
+             << "Year of Birth: " << dob.year << endl
+             << "Living in " << address.city;
     }
     return 0;
 }
