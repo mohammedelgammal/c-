8f4e90517070686884fa515c24eadf4e52cccd79
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-07-28 05:02:24 +0300

    writing_to_csv_files_ofstream

--- index.cpp
+++ index.cpp
@@ -6,11 +6,15 @@ int main()
 
 {
     ofstream file;
-    file.open("first.txt");
+
+    file.open("data.csv");
     if (file.is_open())
     {
-        file << "Hey! I am here!" << endl;
+        file << "id,title,year\n"
+             << "1, Terminator 1, 1984\n"
+             << "2, Terminator 2, 1991\n";
         file.close();
     }
+
     return 0;
 }
