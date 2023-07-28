60658efb66ef8405508862147500bc1d7a450ba5
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-07-28 23:31:18 +0300

    to_string_method_ability_to_manipulate_precision

--- index.cpp
+++ index.cpp
@@ -4,16 +4,18 @@
 
 using namespace std;
 
-void to_double(double num)
+string to_string(double &number, const short precision)
 {
     ostringstream stream;
-    stream << fixed << setprecision(2) << num;
-    cout << stream.str() << endl;
+    stream << fixed << setprecision(precision) << number;
+    string output_string = stream.str();
+    return output_string;
 }
 
 int main()
 {
-    double number = 46.8679442;
-    to_double(number);
+    double double_number = 4.9482;
+    short precision = 2;
+    cout << to_string(double_number, precision);
     return 0;
 }
