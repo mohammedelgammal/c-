cdb7129a5bd158716fce510dfc12b9d276c49573
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-07-31 16:06:17 +0300

    upcasting_child_to_parent

--- main.cpp
+++ main.cpp
@@ -1,15 +1,22 @@
 #include "TextBox.hpp"
+#include "Widget.hpp"
 #include <iostream>
 
 using namespace std;
 
+void display_widget(int *widget)
+{
+    cout << &widget << endl;
+    cout << boolalpha << *(widget).isEnabled() << endl;
+}
+
 int main()
 {
     TextBox box_one{"one"};
+    int box_ptr = &box_one;
 
-    box_one.disable();
-
-    cout << boolalpha << box_one.isEnabled() << endl;
+    cout << &box_one << endl;
+    display_widget(box_ptr);
 
     return 0;
 }
