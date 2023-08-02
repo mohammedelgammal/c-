f102775c464f88d079fd7192de696e7d93f6a112
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-08-03 01:21:05 +0300

    including_header_file_without_extension

--- main.cpp
+++ main.cpp
@@ -1,20 +1,10 @@
 #include <iostream>
-
-namespace math
-{
-    template <typename T>
-    T sumUp(T one, T two)
-    {
-        return one + two;
-    }
-
-}
+#include "SumUp"
 
 int main()
 {
     using math::sumUp, std::cout, std::endl;
 
     cout << sumUp(1, 2) << endl;
-
     return 0;
 }
