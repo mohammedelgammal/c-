243540864aeec2ea795bb7d4fcf96ec37b2323fe
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-08-03 15:29:13 +0300

    using_setw_right_&_left_alignemnt_using_iomanip_header

--- main.cpp
+++ main.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
+#include <iomanip>
+
+using namespace std;
 
 int main()
 {
-    long double numOne = 1.345E6L;
-    double numTwo = 0;
-    float numThree = 0;
+    long pop1 = 21E5, pop2 = 74E3, pop3 = 11E4;
 
-    using std::cout, std::endl;
-
-    cout << sizeof(numOne) << endl
-         << numOne << endl;
+    cout << left << setw(15) << "LOCATION" << right << setw(15) << "POPULATION" << endl
+         << left << setw(15) << "Mansoura" << right << setw(15) << pop1 << endl
+         << left << setw(15) << "Gharbia" << right << setw(15) << pop2 << endl
+         << left << setw(15) << "Sohag" << right << setw(15) << pop3 << endl;
 
     return 0;
 }
