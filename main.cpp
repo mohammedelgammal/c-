e6920c984ca88756ee2612948f5d975cff44f4b3
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-08-03 16:08:37 +0300

    using_unsigned_variables_to_calculate_numbers_may_exceed_data_type_limits_as_it_has_double_positive_capacity

--- main.cpp
+++ main.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
-#include <iomanip>
-
-using namespace std;
 
 int main()
 {
-    long pop1 = 21E5, pop2 = 74E3, pop3 = 11E4;
+    int signedVar = 2E9;
+    unsigned int unsignedVar = 2E9;
 
-    cout << left << setw(15) << "LOCATION" << right << setw(15) << "POPULATION" << endl
-         << left << setw(15) << "Mansoura" << right << setw(15) << pop1 << endl
-         << left << setw(15) << "Gharbia" << right << setw(15) << pop2 << endl
-         << left << setw(15) << "Sohag" << right << setw(15) << pop3 << endl;
+    using std::cout, std::endl;
+
+    cout << "signedVar: " << signedVar * 2 / 3 << endl
+         << "unsignedVar: " << unsignedVar * 2 / 3 << endl;
 
     return 0;
 }
