082acc868be51d73e20890001f15e82e363e9abe
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-08-07 19:27:25 +0300

    initailzing_&_declaring_defining

--- main.cpp
+++ main.cpp
@@ -1,67 +1,21 @@
 #include <iostream>
-#include <iomanip>
 
 using namespace std;
 
-int getInputNumber()
-{
-    int number = 0;
-
-    for (;;)
-    {
-        cout << "Enter target number: " << endl;
-        cin >> number;
-        if (!cin.fail())
-            break;
-        cin.clear();
-        cin.ignore(numeric_limits<int>::max(), '\n');
-    }
-
-    return number;
-}
-
-unique_ptr<vector<int>> getMultiples(int number, const int cols, const int rows)
-{
-    unique_ptr<vector<int>> multiples = make_unique<vector<int>>();
-    const int elementsCount = cols * rows;
-    int count = 1;
-
-    while (count <= elementsCount)
-    {
-        multiples->push_back(number * count);
-        count++;
-    }
-
-    return multiples;
-}
-
-void displayMultiples(unique_ptr<vector<int>> &multiples, int cols)
-{
-    int count = 0;
-    for (int number : *multiples)
-    {
-        if (count % cols == 0)
-        {
-            cout << '\n';
-        }
-        cout << setw(5) << number;
-        count++;
-    }
-}
-
-void displayMultipleGrid(int number, int cols, int rows)
-{
-    cout << number || getInputNumber();
-    int targetNumber = number ? number : getInputNumber();
-    unique_ptr<vector<int>> multiples = getMultiples(targetNumber, cols, rows);
-    displayMultiples(multiples, cols);
-}
+// declaring a function
+// telling the compiler about the variable
+// reserve the variable a space in the memory
+// int cToFahrenheit();
+// initializing a variabe
+// set the function to an inital value
+// cToFahrenheit = () {
+// }
 
 int main()
 {
-    int number = 7, cols = 20, rows = 5;
+    char isko;
 
-    displayMultipleGrid(number, cols, rows);
+    cout << isko << endl;
 
     return 0;
 }
