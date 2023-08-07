3abfbe491ab86ec818bbfd475f2889bbb8593ad8
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-08-08 01:21:08 +0300

    assigning_a_const_value_to_variable_reference

--- main.cpp
+++ main.cpp
@@ -1,22 +1,14 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-// declaring a function
-// telling the compiler about the variable
-// reserve the variable a space in the memory
-inline void cToFahrenheit();
-// initializing a variabe
-// set the function to an inital value
+int& returnStringRef() {
+    return 7;
+}
 
 int main()
 {
-    cToFahrenheit();
-
+    int& stringRef = returnStringRef();
     return 1;
-}
-
-void cToFahrenheit()
-{
-    cout << "function defined here! and declared before the main!" << endl;
 }
