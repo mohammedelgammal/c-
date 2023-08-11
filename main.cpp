1c1e0ae473c73a216f518e9aaab5687c1c773e8e
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-08-11 23:08:03 +0300

    using_explicit_conversion_with_only_one_arg_constructor_either_than_this_error_takes_one_have_two

--- main.cpp
+++ main.cpp
@@ -2,18 +2,26 @@
 #include "Distance"
 
 using namespace std;
+void callDistance(Distance &distance)
+{
+    cout << "Function km: " << distance.getKiloMeters() << endl;
+    cout << "Function meters: " << distance.getMeters() << endl;
+}
 
 int main()
 {
+
     Distance distanceOne(2, 238);
 
-    cout << "KiloMeters: " << distanceOne.getKiloMeters() << endl;
-    cout << "Meters: " << distanceOne.getMeters() << endl;
+    // cout << "KiloMeters: " << distanceOne.getKiloMeters() << endl;
+    // cout << "Meters: " << distanceOne.getMeters() << endl;
 
-    distanceOne = 15.625;
+    // // distanceOne = 15.625;
 
-    cout << "Float distance: " << distanceOne.getKiloMeters() << endl;
-    cout << "Float distance: " << distanceOne.getMeters() << endl;
+    // cout << "Float distance: " << distanceOne.getKiloMeters() << endl;
+    // cout << "Float distance: " << distanceOne.getMeters() << endl;
+
+    callDistance(2, 321);
 
     return 0;
 }
