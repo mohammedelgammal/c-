ffe777a7859085fc0894efd90437122d3f704f5f
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-08-15 01:28:42 +0300

    display_different_types_pointers_using_refernce-to_operator_&_reinterpret_cast_to _interpret_char_refernce_variable_using_stream_input_operator

--- main.cpp
+++ main.cpp
@@ -1,44 +1,15 @@
 #include <iostream>
 using namespace std;
 
-class A
-{
-public:
-    void show()
-    {
-        cout << "Hello from A \n";
-    }
-};
-
-class B : virtual public A
-{
-public:
-    // void show()
-    // {
-    //     cout << "Hello from B" << endl;
-    // }
-};
-
-class C : virtual public A
-{
-public:
-    // void show()
-    // {
-    //     cout << "Hello from C" << endl;
-    // }
-};
-
-class D : public B, public C
-{
-public:
-    // void show()
-    // {
-    //     cout << "Hello from D" << endl;
-    // }
-};
-
 int main()
 {
-    D object;
-    object.show();
+    int var1, var2;
+    char char1;
+    double dob1;
+
+    cout << &var1 << " - " << &var2 << endl;
+    cout << reinterpret_cast<void *>(&char1) << endl;
+    cout << &dob1 << endl;
+
+    return 0;
 }
