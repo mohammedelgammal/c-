90829830234c3bfc9d7123174027113f1654b8db
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-08-17 01:33:12 +0300

    adding_friend_function_that_has_access_to_two_classes_member_variable_at_same_time_just_as_comon_friend_to_both_alph_&_beta

---
+++ AlphaBeta.cpp
@@ -0,0 +1,6 @@
+#include "AlphaBeta"
+
+int sumUp(Alpha *alpha, Beta *beta)
+{
+    return alpha->value + beta->value;
+}
--- main.cpp
+++ main.cpp
@@ -1,19 +1,15 @@
 #include <iostream>
+#include "AlphaBeta"
+
 using namespace std;
 
 int main()
 {
-    int num = 3, numTwo = 5;
-    const int *ptrcint; // pointer to a constant integer
 
-    ptrcint = &num;
-    ptrcint = &numTwo; // can make point to another int
-    numTwo = 4;
+    Alpha alpha;
+    Beta beta;
 
-    ptrcint = &numTwo;
-
-    int *const cptrint = &num; // constant pointer to an integer
-    // cptrint = &numTwo; // can point only to one int so it needs an initializer
+    cout << sumUp(&alpha, &beta) << endl;
 
     return 0;
 }
