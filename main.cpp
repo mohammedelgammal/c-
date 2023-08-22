eae958e968d56b1996edeb635935e38748ed1419
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-08-23 01:52:39 +0300

    declaring_template_&_using_in_class_declaration

--- main.cpp
+++ main.cpp
@@ -3,19 +3,19 @@
 
 using namespace std;
 
-int Beta::total = 0;
+// int Beta::total = 0;
 
 int main()
 {
     Alpha alpha;
-    Beta beta;
+    // Beta beta;
 
     extern int global_var;
     int sum(int a, int b);
 
     int age;
 
-    Beta::getInstances();
+    // Beta::getInstances();
 
     cout << "global: " << sum(1, 4) << endl;
 
