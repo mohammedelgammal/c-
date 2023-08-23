24a62fb75e68300b6f04e435ff2c8913c9313aca
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-08-24 00:34:13 +0300

    using_normal_iterator_to_traverse_vector::iterator

--- main.cpp
+++ main.cpp
@@ -1,52 +1,21 @@
-#include <ostream>
-#include "AlphaBeta"
-#include <string>
-#include <algorithm>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-// int Beta::total = 0;
-
 int main()
 {
-    Alpha alpha;
-    // Beta beta;
+    vector<int> vec{1, 2, 3, 4, 5};
 
-    extern int global_var;
-    int sum(int a, int b);
-    string name = "howard";
-    int arr[7]{5, 8, 4, 1, 7, 9, 3};
+    vector<int>::iterator ite = vec.begin();
 
-    int age;
+    cout << "Second vector element is: " << *(++ite) << endl;
 
-    char fillArr[10];
-
-    fill(fillArr, fillArr + 10, 0);
-
-    cout << "Filled Array is: ";
-
-    for (int num : fillArr)
+    // using normal increment iterators to traverse the vector vec
+    for (vector<int>::iterator ite = vec.begin(); ite < vec.end(); ++ite)
     {
-        cout << num;
+        cout << *ite;
     }
-
-    cout << '\n';
-
-    // Beta::getInstances();
-
-    cout
-        << "global: " << sum(1, 4) << endl;
-
-    sort(arr, arr + 7);
-
-    cout << "Sorted array is: ";
-
-    for (int num : arr)
-    {
-        cout << num;
-    }
-
-    cout << "\nfirst character in name is: " << name.front() << endl;
 
     return 0;
 }
