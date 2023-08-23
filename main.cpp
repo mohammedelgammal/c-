fd2464b38fd0665986190f39844df609a2da4148
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-08-24 02:08:16 +0300

    implementing_count_algorithm_to_array_built-in_data_structure

--- main.cpp
+++ main.cpp
@@ -6,9 +6,10 @@ using namespace std;
 
 int main()
 {
-    int arr[5] = {1, 2, 3, 4, 5};
+    int arr[6] = {1, 2, 3, 4, 5, 5};
 
     cout << *find(arr, arr + 8, 4) << endl;
+    cout << "Number 5 found: " << count(arr, arr + 7, 5) << endl;
 
     return 0;
 }
