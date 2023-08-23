c2c88580464cbc4dc6538b2ffd912aaef0ce6cbe
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-08-24 01:51:30 +0300

    using_istream_operator_to_input_iteration

--- main.cpp
+++ main.cpp
@@ -15,12 +15,16 @@ int main()
 
     cout << "Second to last element is: " << *(++rite) << endl;
 
-    // using the random access iterator to loop through the vector
-    while (raite < vec.end())
+    // using input iterator to iterate through input stream
+    cout << "Please enter as many chars as you want (Ctrl + D) to exit!" << endl;
+
+    istream_iterator<int> iite(std::cin);
+    istream_iterator<int> end;
+
+    while (iite != end)
     {
-        cout << *raite;
-        // ++raite;
-        advance(raite, 2); 
+        vec.push_back(*iite);
+        ++iite;
     }
 
     return 0;
