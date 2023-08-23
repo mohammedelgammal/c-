47982273a22a905b5fb6d61b1d959747399c30b7
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-08-24 02:13:14 +0300

    implementing_sort_algorithm_in_unarranged_array

--- main.cpp
+++ main.cpp
@@ -7,9 +7,17 @@ using namespace std;
 int main()
 {
     int arr[6] = {1, 2, 3, 4, 5, 5};
+    int unarranged_arr[] = {45, 2, 22, -17, 0, -30, 25, 55};
 
     cout << *find(arr, arr + 8, 4) << endl;
     cout << "Number 5 found: " << count(arr, arr + 7, 5) << endl;
+
+    sort(unarranged_arr, unarranged_arr + 8);
+
+    for (int i = 0; i < 8; i++)
+    {
+        cout << unarranged_arr[i] << "\n";
+    }
 
     return 0;
 }
