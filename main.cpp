39a4a15861004bd554569aae88e715e2eaa5690e
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-08-24 00:48:42 +0300

    using_bidirectional_iterator_to_iterate_reversely_on_vector

--- main.cpp
+++ main.cpp
@@ -8,13 +8,16 @@ int main()
     vector<int> vec{1, 2, 3, 4, 5};
 
     vector<int>::iterator ite = vec.begin();
+    vector<int>::reverse_iterator rite = vec.rbegin();
 
     cout << "Second vector element is: " << *(++ite) << endl;
 
-    // using normal increment iterators to traverse the vector vec
-    for (vector<int>::iterator ite = vec.end(); ite > --vec.begin(); --ite)
+    cout << "Second to last element is: " << *(++rite) << endl;
+
+    // using bidirectional iterator to loop through the vector in a reverse way
+    for (vector<int>::reverse_iterator rite = vec.rbegin(); rite < vec.rend(); ++rite)
     {
-        cout << *ite;
+        cout << *rite;
     }
 
     return 0;
