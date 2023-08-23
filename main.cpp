a7080aec698af170fb95d82cd21974478dc4c1c9
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-08-24 02:01:20 +0300

    using_find_alogrithm_on_array

--- main.cpp
+++ main.cpp
@@ -1,31 +1,14 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main()
 {
-    vector<int> vec{1, 2, 3, 4, 5};
+    int arr[5] = {1, 2, 3, 4, 5};
 
-    vector<int>::iterator ite = vec.begin();
-    vector<int>::reverse_iterator rite = vec.rbegin();
-    vector<int>::iterator raite = vec.begin();
-
-    cout << "Second vector element is: " << *(++ite) << endl;
-
-    cout << "Second to last element is: " << *(++rite) << endl;
-
-    // using input iterator to iterate through input stream
-    cout << "Please enter as many chars as you want (Ctrl + D) to exit!" << endl;
-
-    istream_iterator<int> iite(std::cin);
-    istream_iterator<int> end;
-
-    while (iite != end)
-    {
-        vec.push_back(*iite);
-        ++iite;
-    }
+    cout << *find(arr, arr + 8, 4) << endl;
 
     return 0;
 }
