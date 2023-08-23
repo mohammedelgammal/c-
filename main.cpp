e68fc1999d2a659a174b123c4c436d1c6ddcdf59
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-08-24 02:22:15 +0300

    implementing_search_algorithm_to_find_start_of_matching_including_array

--- main.cpp
+++ main.cpp
@@ -8,9 +8,21 @@ int main()
 {
     int arr[6] = {1, 2, 3, 4, 5, 5};
     int unarranged_arr[] = {45, 2, 22, -17, 0, -30, 25, 55};
+    int incl_arr[] = {3, 4, 5};
 
     cout << *find(arr, arr + 8, 4) << endl;
     cout << "Number 5 found: " << count(arr, arr + 7, 5) << endl;
+
+    int matching_index = *search(unarranged_arr, unarranged_arr + 2, arr, arr + 6);
+
+    if (matching_index)
+    {
+        cout << "matching start number is: " << matching_index << endl;
+    }
+    else
+    {
+        cout << "No matching found!" << endl;
+    }
 
     sort(unarranged_arr, unarranged_arr + 8);
 
