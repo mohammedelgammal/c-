c05eb9a3b169cb1f481751a88f265c22a0713943
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-08-24 03:25:43 +0300

    implementing_char_arrays_comparison_using_user_predefined_function_object_functor

--- main.cpp
+++ main.cpp
@@ -13,14 +13,22 @@ int main()
             return a > b;
         }
     };
+    struct char_compare
+    {
+        bool operator()(char *a, char *b)
+        {
+            return strcmp(a, b) < 0 ? true : false;
+        }
+    };
 
     int arr[]{1, 2, 3, 4, 5};
+    char *names[]{"mohammed", "mahmoud", "ali", "amr", "amir"};
 
-    sort(arr, arr + 5, greater<>());
+    sort(names, names + 5, char_compare());
 
-    for (int num : arr)
+    for (char *name : names)
     {
-        cout << num;
+        cout << name << " ";
     }
 
     return 0;
