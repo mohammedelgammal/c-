640a593b03dd92388f8c4906d176738b6656e9c1
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-09-08 21:49:08 +0300

    adding_addFirst_feature_to_add_node_at_front_end

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,10 +1,34 @@
 #include "LinkedList"
+#include <iostream>
+
+using namespace std;
 
 Node::Node(const int value, Node *next) : value{value}, next{next}
 {
 }
 
-LinkedList::LinkedList(Node *head) : head{head}
+LinkedList::LinkedList(int value) : head{new Node(value, nullptr)}
 {
 }
 
+LinkedList::LinkedList() : head{nullptr}
+{
+}
+
+LinkedList::~LinkedList()
+{
+    delete head;
+    head = nullptr;
+};
+
+void LinkedList::addFirst(const int value)
+{
+    Node *newHead = new Node(value, nullptr);
+    if (head == nullptr)
+    {
+        head = newHead;
+        return;
+    }
+    newHead->next = head;
+    head = newHead;
+}
--- main.cpp
+++ main.cpp
@@ -5,10 +5,9 @@ using namespace std;
 
 int main()
 {
-    Node head{1, nullptr};
-    Node *head_ptr = &head;
+    LinkedList list;
 
-    LinkedList newList{head_ptr};
+    list.addFirst(3);
 
     return 0;
 }
