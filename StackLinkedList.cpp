1557479e18b28d0abda81067d2cfb438feda44e7
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-09-12 23:44:06 +0300

    implementing_all_linked_list_based_stack_methods_push_pop_getPeak_isEmpty

--- StackLinkedList.cpp
+++ StackLinkedList.cpp
@@ -7,7 +7,7 @@ StackLinkedList<T>::StackLinkedList() : stack{new LinkedList<T>}
 }
 
 template <typename T>
-StackLinkedList<T>::StackLinkedList(T top) : stack{new LinkedList<T>{top}}, top{top}
+StackLinkedList<T>::StackLinkedList(T top) : stack{new LinkedList<T>{top}}
 {
 }
 
@@ -16,4 +16,28 @@ StackLinkedList<T>::~StackLinkedList()
 {
     delete stack;
     stack = nullptr;
+}
+
+template <typename T>
+void StackLinkedList<T>::push(const T value)
+{
+    stack->addLast(value);
+}
+
+template <typename T>
+T StackLinkedList<T>::pop()
+{
+    stack->deleteLast();
+}
+
+template <typename T>
+T StackLinkedList<T>::getPeak() const
+{
+    return stack->getKthFromEnd(1);
+}
+
+template <typename T>
+bool StackLinkedList<T>::isEmpty() const
+{
+    return stack->isListEmpty();
 }
