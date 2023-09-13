2b71fb44b18c136b93c1066616e01af9c43efa21
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-09-13 03:45:43 +0300

    adding_enqueue_dequeue_isEmpty_getPeak_getLength_&_overloading_[]_operator_to_queue

--- Queue.cpp
+++ Queue.cpp
@@ -18,3 +18,47 @@ Queue<T>::~Queue()
     delete stack;
     stack = nullptr;
 }
+
+template <typename T>
+void Queue<T>::enqueue(const T value)
+{
+    stack->insert(value);
+}
+
+template <typename T>
+T Queue<T>::dequeue()
+{
+    const T lastNode = (*stack)[0];
+    stack->removeAt(0);
+    return lastNode;
+}
+
+template <typename T>
+T Queue<T>::getPeak() const
+{
+    return (*stack)[stack->length - 1];
+}
+
+template <typename T>
+T Queue<T>::getBottom() const
+{
+    return (*stack)[0];
+}
+
+template <typename T>
+bool Queue<T>::isEmpty() const
+{
+    return stack->length == 0;
+}
+
+template <typename T>
+int Queue<T>::getLength() const
+{
+    return stack->length;
+}
+
+template <typename T>
+T Queue<T>::operator[](const int index) const
+{
+    return (*stack)[index];
+}
--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 
 // Data Structures
 #include "Queue"
+#include "Queue.cpp"
 
 using namespace std;
 
@@ -10,5 +11,28 @@ int main()
 {
     Queue<int> queue;
 
-        return 0;
+    queue.enqueue(3);
+    queue.enqueue(4);
+    queue.enqueue(5);
+    queue.enqueue(6);
+
+    // queue.dequeue();
+    // queue.dequeue();
+    // queue.dequeue();
+    // queue.dequeue();
+
+    // cout << queue.getPeak() << endl;
+
+    // cout << queue.getBottom() << endl;
+
+    // cout << queue.getLength() << endl;
+
+    // cout << boolalpha << queue.isEmpty() << endl;
+
+    for (int i = 0; i < queue.getLength(); i++)
+    {
+        cout << queue[i] << endl;
+    }
+
+    return 0;
 }
