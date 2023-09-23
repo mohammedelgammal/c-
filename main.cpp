36fd36b75f536d3f0d1eba1b6aea246b20d7716a
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-09-23 05:56:44 +0300

    implementing_find_first_non_repeating_character_of_string_using_hashtable_lookup

--- main.cpp
+++ main.cpp
@@ -1,17 +1,40 @@
 // Libraries
 #include <iostream>
-#include <string>
-#include <typeinfo>
-
-// Data Structures
-#include "HashTable"
-#include "HashTable.cpp"
 
 using namespace std;
 
+char getFirstUnrepeatedChar(string &str)
+{
+    char targetChar;
+    unordered_map<char, int> hashTable;
+
+    for (char chr : str)
+    {
+        if (hashTable[chr] == 0)
+            hashTable[chr] = 1;
+        else
+            hashTable[chr] = 2;
+    }
+
+    for (char chr : str)
+    {
+        if (hashTable[chr] == 1)
+        {
+            targetChar = chr;
+            break;
+        }
+    }
+
+    return targetChar;
+}
+
 int main()
 {
-    
+    unordered_map<string, string> hashTable;
+    string targetString = "ggreen book";
+
+    // getFirstUnrepeatedChar(targetString);
+    cout << getFirstUnrepeatedChar(targetString) << endl;
 
     return 0;
 }
