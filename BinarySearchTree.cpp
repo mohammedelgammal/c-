392d93eac4717f1aae7b21fea1d58609f735e630
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-09-24 14:34:44 +0300

    implementing_insert_&_find_methods_to_bsearchtree

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -1,4 +1,7 @@
 #include "BinarySearchTree"
+#include <iostream>
+
+using std::cout, std::endl;
 
 BinarySearchTree::BinarySearchTree(const int value) : root{new Node{value}}
 {
@@ -10,3 +13,57 @@ BinarySearchTree::~BinarySearchTree()
     root = nullptr;
 }
 
+void BinarySearchTree::insert(const int value)
+{
+    Node *newNode = new Node{value};
+    Node *currentNode = root;
+
+    while (currentNode)
+    {
+        if (newNode->value > currentNode->value)
+        {
+            if (currentNode->rightChildNode == nullptr)
+            {
+                currentNode->rightChildNode = newNode;
+                newNode->parentNode = currentNode;
+                return;
+            }
+            currentNode = currentNode->rightChildNode;
+        }
+        if (newNode->value < currentNode->value)
+        {
+            if (currentNode->leftChildNode == nullptr)
+            {
+                currentNode->leftChildNode = newNode;
+                newNode->parentNode = currentNode;
+                return;
+            }
+            currentNode = currentNode->leftChildNode;
+        }
+    }
+
+    delete currentNode;
+    delete newNode;
+    currentNode = newNode = nullptr;
+}
+
+bool BinarySearchTree::find(const int value) const
+{
+    Node *currentNode = root;
+
+    while (currentNode)
+    {
+        if (value == currentNode->value)
+            return true;
+        if (value > currentNode->value)
+        {
+            currentNode = currentNode->rightChildNode;
+        }
+        else
+        {
+            currentNode = currentNode->leftChildNode;
+        }
+    }
+    
+    return false;
+}
--- main.cpp
+++ main.cpp
@@ -9,5 +9,18 @@ int main()
 {
     BinarySearchTree tree{10};
 
+    tree.insert(7);
+    tree.insert(9);
+    tree.insert(5);
+    tree.insert(4);
+    tree.insert(6);
+
+    cout << boolalpha << tree.find(7) << endl
+         << tree.find(9) << endl
+         << tree.find(5) << endl
+         << tree.find(4) << endl
+         << tree.find(10) << endl
+         << tree.find(13) << endl;
+
     return 0;
 }
