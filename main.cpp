125715664845b308bbb23c3f0d3b5a1f8beae994
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-10-02 02:30:11 +0300

    implementing_balance_factor_isLeftHeavy_&right_to_get_to_balancing_tree

--- main.cpp
+++ main.cpp
@@ -9,12 +9,11 @@ int main()
 {
     AVLTree tree{10};
 
-    tree.insert(15);
     tree.insert(5);
-    tree.insert(20);
-    tree.insert(12);
-    tree.insert(7);
+    tree.insert(15);
     tree.insert(3);
+    tree.insert(1);
+    tree.insert(20);
 
     tree.levelOrderTreeTraverse();
 
