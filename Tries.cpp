11e558f49630985c2a21c6a2dea1efef034c9413
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-10-04 03:14:37 +0300

    initializing_tries_class_&_implementing_insertion_method

---
+++ Tries.cpp
@@ -0,0 +1,29 @@
+#include "Tries"
+#include <iostream>
+
+using std::vector, std::string;
+
+Tries::Tries() : root{new Node}
+{
+}
+
+Tries::~Tries()
+{
+    delete root;
+    root = nullptr;
+}
+
+void Tries::insert(const string word)
+{
+    Node *current = root;
+    int charIndex;
+
+    for (char character : word)
+    {
+        charIndex = character - 'a';
+        if (!current->children.at(charIndex)->character)
+            current->children.at(charIndex) = new Node{character};
+
+        current = current->children.at(charIndex);
+    }
+}
--- main.cpp
+++ main.cpp
@@ -1,54 +1,12 @@
 // Libraries
 #include <iostream>
+#include "Tries"
 
 using namespace std;
 
-int leftChildIndex(const int index)
-{
-    return (index * 2) + 1;
-}
-
-int rightChildIndex(const int index)
-{
-    return (index * 2) + 2;
-}
-
-int greaterChildIndex(vector<int> &arr, const int index)
-{
-    return arr[leftChildIndex(index)] > arr[rightChildIndex(index)]
-               ? leftChildIndex(index)
-               : rightChildIndex(index);
-}
-
-void bubbleDown(vector<int> &arr, int index)
-{
-    while (greaterChildIndex(arr, index) < arr.size() &&
-           arr[index] < arr[greaterChildIndex(arr, index)])
-    {
-        int current = arr[index];
-        arr[index] = arr[greaterChildIndex(arr, index)];
-        arr[greaterChildIndex(arr, index)] = current;
-        index = greaterChildIndex(arr, index);
-    }
-}
-
-void heapify(vector<int> &arr)
-{
-    if (arr.empty())
-        return;
-
-    for (int i = 0; i < arr.size(); i++)
-        bubbleDown(arr, i);
-}
-
 int main()
 {
-    vector<int> arr{5, 3, 8, 4, 1, 2};
-
-    heapify(arr);
-
-    for (int i = 0; i < arr.size(); i++)
-        cout << arr[i] << '|';
+    Tries tries;
 
     return 0;
 }
