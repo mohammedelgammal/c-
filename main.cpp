05c89da4bb615cfc3f979feffa15d0f435ecebd8
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-10-15 03:23:19 +0300

    implementing_quickSort_algorithm

--- main.cpp
+++ main.cpp
@@ -4,46 +4,42 @@
 
 using namespace std;
 
-void merge(vector<int> &left, vector<int> &right, vector<int> &unsorted)
+void quickSort(vector<int> &unsorted)
 {
-    int i = 0, j = 0, k = 0;
-
-    while (i < left.size() && j < right.size())
-    {
-        if (left[i] < right[j])
-            unsorted[k++] = left[i++];
-        else
-            unsorted[k++] = right[j++];
-    }
-
-    while (i < left.size())
-        unsorted[k++] = left[i++];
-
-    while (j < right.size())
-        unsorted[k++] = right[j++];
-}
-
-void mergeSort(vector<int> &unsorted)
-{
-    if (unsorted.size() < 2)
+    if (unsorted.size() <= 1)
         return;
 
-    int middleIndex = unsorted.size() / 2;
+    int pivot = unsorted.back(),
+        boundary = -1,
+        i = 0;
 
-    vector<int> left{unsorted.begin(), (unsorted.begin() + middleIndex)},
-        right{(unsorted.begin() + middleIndex), unsorted.end()};
+    for (int i = 0; i < unsorted.size(); i++)
+    {
+        if (unsorted[i] <= pivot)
+        {
+            int temp = unsorted[i];
 
-    mergeSort(left);
-    mergeSort(right);
+            unsorted[i] = unsorted[++boundary];
+            unsorted[boundary] = temp;
+        }
+    }
 
-    merge(left, right, unsorted);
+    vector<int> left = {unsorted.begin(), (unsorted.begin() + boundary)},
+                right = {(unsorted.begin() + boundary), unsorted.end()};
+
+    quickSort(left);
+    quickSort(right);
+
+    // merge left & right by adding right to left
+    left.insert(left.end(), right.begin(), right.end());
+    unsorted = left;
 }
 
 int main()
 {
     vector<int> unsorted{7, 3, 1, 4, 6, 2, 3};
 
-    mergeSort(unsorted);
+    quickSort(unsorted);
 
     for (int i = 0; i < unsorted.size(); i++)
         cout << unsorted[i];
