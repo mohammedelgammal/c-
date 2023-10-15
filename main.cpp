e7b050b63e232361b7f2809fa6f7442bac5cb957
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-10-15 21:17:46 +0300

    implementing_linearSearch_algorithm

--- main.cpp
+++ main.cpp
@@ -4,50 +4,23 @@
 
 using namespace std;
 
-void bubbleSort(vector<int> &unsorted)
+int linearSearch(vector<int> &unsorted, int target)
 {
-    int size = unsorted.size();
-
-    for (int i = 0; i < size; i++)
-        for (int j = 0; j < size; j++)
-            if (unsorted[i] < unsorted[j])
-            {
-                int temp = unsorted[i];
-                unsorted[i] = unsorted[j];
-                unsorted[j] = temp;
-            }
-}
-
-void bucketSort(vector<int> &unsorted)
-{
-    vector<vector<int>> buckets;
-    vector<int> sorted;
-
-    buckets.assign(3, vector<int>{});
-
     for (int i = 0; i < unsorted.size(); i++)
     {
-        int bucketIndex = unsorted[i] / 3;
-        buckets[bucketIndex].push_back(unsorted[i]);
+        if (unsorted[i] == target)
+            return i;
     }
-
-    for (int j = 0; j < buckets.size(); j++)
-        bubbleSort(buckets[j]);
-
-    for (int k = 0; k < buckets.size(); k++)
-        sorted.insert(sorted.end(), buckets[k].begin(), buckets[k].end());
-
-    unsorted = sorted;
+    return -1;
 }
 
 int main()
 {
     vector<int> unsorted{7, 3, 1, 4, 6, 2, 3};
 
-    bucketSort(unsorted);
+    int result = linearSearch(unsorted, 0);
 
-    for (int i = 0; i < unsorted.size(); i++)
-        cout << unsorted[i];
+    cout << result << endl;
 
     return 0;
 }
