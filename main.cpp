4a8506ee4227f5dd18e823010a7478f8e74235b3
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-10-17 00:55:50 +0300

    implementing_reverse_stringusing_improved_O(n)_instead_of_O(2n)

--- main.cpp
+++ main.cpp
@@ -9,17 +9,10 @@ void reverse(string &input)
     if (input.length() == 0)
         return;
 
-    stack<char> stack;
     string reversed;
 
-    for (int i = 0; i < input.length(); i++)
-        stack.push(input[i]);
-
-    for (int i = 0; i < input.length(); i++)
-    {
-        reversed += stack.top();
-        stack.pop();
-    }
+    for (int i = input.length() - 1; i >= 0; i--)
+        reversed += input[i];
 
     input = reversed;
 }
