b6f2afd5bf3b69321dae78215532802893b00813
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-10-17 00:36:26 +0300

    adding_i_to_vowels

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int countVowels(string &input)
     if (input.length() == 0)
         return 0;
 
-    set<char> vowels{'a', 'e', 'o', 'u'};
+    set<char> vowels{'a', 'e', 'o', 'u', 'i'};
     int vowelsCount = 0;
 
     for (int i = 0; i < input.length(); i++)
