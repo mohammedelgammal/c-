191b90f06c501480dc1aa0fa3593d14a1dedcac1
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-10-17 14:26:39 +0300

    implementing_way_easier_way_to_check_for_rotations

--- main.cpp
+++ main.cpp
@@ -6,41 +6,16 @@ using namespace std;
 
 bool areRotations(string &first, string &second)
 {
-    if (first == second)
-        return true;
-
-    if (first.length() != second.length())
+    if (!first.length() || !second.length())
         return false;
 
-    int i = 0, j = 0;
-    bool areRotations = false;
-
-    while (i < first.length())
-    {
-        if (j >= first.length())
-            j = 0;
-
-        if (first[i] == second[j])
-        {
-            areRotations = true;
-            i++;
-        }
-        else
-        {
-            areRotations = false;
-
-            if (i != 0)
-                return false;
-        }
-        j++;
-    }
-
-    return areRotations;
+    return (first.length() == second.length()) &&
+           (first.append(first).find(second) != string::npos);
 }
 
 int main()
 {
-    string input = "ABCD", rotation = "DBAC";
+    string input = "ABCD", rotation = "BCADa";
 
     cout << boolalpha << areRotations(input, rotation);
 
