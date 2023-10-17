b1e966288a88ed2595e04eba3aa50cb8b44ca1ec
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-10-17 16:58:39 +0300

    implementing_toUpperCase_algorithm

--- main.cpp
+++ main.cpp
@@ -3,30 +3,30 @@
 
 using namespace std;
 
-char findMostRepeatedChar(string &input)
+void capitalize(string &input)
 {
-    vector<int> asciArray(256);
-    int maxRepitions = 0;
-    char maxCharacter;
+    int startIndex = 0, endIndex = 0;
+    int CASE_DIFFERENCE = 32;
 
-    for (char character : input)
-        asciArray[character]++;
-
-    for (int i = 0; i < asciArray.size(); i++)
-        if (asciArray[i] > maxRepitions)
+    while (endIndex < input.length())
+    {
+        if (input[endIndex] == ' ' || endIndex >= input.length() - 1)
         {
-            maxRepitions = asciArray[i];
-            maxCharacter = i;
+            if (input[startIndex] != ' ' && input[startIndex] >= 97)
+                input[startIndex] -= CASE_DIFFERENCE;
+            startIndex = endIndex + 1;
         }
-
-    return maxCharacter;
+        endIndex++;
+    }
 }
 
 int main()
 {
-    string input = "hello";
+    string input = "  hello there,   Good mornning!  ";
 
-    cout << findMostRepeatedChar(input);
+    capitalize(input);
+
+    cout << input;
 
     return 0;
 }
