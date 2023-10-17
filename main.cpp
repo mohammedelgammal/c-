8ddc74d5ce9e3ad5562a38c546ef1bd36d14cee7
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-10-18 01:30:37 +0300

    starting_leetcode_problems

--- main.cpp
+++ main.cpp
@@ -4,21 +4,8 @@
 
 using namespace std;
 
-bool isPalindrome(string &input)
-{
-    string reversed;
-
-    for (int i = input.length() - 1; i >= 0; i--)
-        reversed.push_back(input[i]);
-
-    return reversed == input;
-}
-
 int main()
 {
-    string input = "racecare";
-
-    cout << boolalpha << isPalindrome(input);
 
     return 0;
 }
