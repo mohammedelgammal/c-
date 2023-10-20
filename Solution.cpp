7ce6a01f8d2192e5401560cdb3ef88cba98692d5
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-10-20 21:51:43 +0300

    implementing_quickSort_alogorithm_in_a_simpler_way

--- Solution.cpp
+++ Solution.cpp
@@ -7,27 +7,5 @@ using namespace std;
 
 int Solution::majorityElement(vector<int> &nums)
 {
-    unordered_map<int, int> map;
-    int major, max = 0;
-
-    for (int i = 0; i < nums.size(); i++)
-    {
-        if (map.contains(nums[i]))
-            map.find(nums[i])->second++;
-        else
-            map.insert({nums[i], 1});
-    }
-
-    for (int j = 0; j < nums.size(); j++)
-    {
-        unordered_map<int, int>::iterator iter = map.find(nums[j]);
-
-        if (map.find(nums[j])->second > max)
-        {
-            max = iter->second;
-            major = iter->first;
-        }
-    }
-
-    return major;
+    return 0;
 }
--- main.cpp
+++ main.cpp
@@ -5,13 +5,45 @@
 
 using namespace std;
 
+void swap(vector<int> &unsorted, int &boundary, int i)
+{
+    int temp = unsorted[boundary];
+
+    unsorted[boundary] = unsorted[i];
+    unsorted[i] = temp;
+}
+
+void quickSort(vector<int> &unsorted, int startIndex, int endIndex)
+{
+    if (endIndex - startIndex <= 0)
+        return;
+
+    int pivot = unsorted[endIndex],
+        boundary = startIndex - 1;
+
+    for (int i = startIndex; i <= endIndex; i++)
+        if (unsorted[i] <= pivot)
+            swap(unsorted, ++boundary, i);
+
+    quickSort(unsorted, startIndex, boundary - 1);
+    quickSort(unsorted, boundary + 1, endIndex);
+}
+
+void quickSort(vector<int> &unsorted)
+{
+    quickSort(unsorted, 0, unsorted.size() - 1);
+}
+
 int main()
 {
     Solution solution;
 
-    vector<int> unsorted{2, 2, 1};
+    vector<int> unsorted{5, 2, 1, 3, 6, 9, 8, 4, 2, 5, 1, 3, 6, 9};
 
-    cout << solution.majorityElement(unsorted);
+    quickSort(unsorted);
+
+    for (int i = 0; i < unsorted.size(); i++)
+        cout << unsorted[i] << ", ";
 
     return 0;
 }
