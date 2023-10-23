8ddfb048ed7bdb2b92daf56f96d88bf736e9cea1
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-10-24 02:42:23 +0300

    implementing_isAnagram_prob lem_solution_sort

--- Solution.cpp
+++ Solution.cpp
@@ -7,26 +7,11 @@ using namespace std;
 
 bool Solution::isAnagram(string s, string t)
 {
-    unordered_map<char, int> map1;
-
     if (s.length() != t.length())
         return false;
 
-    for (int i = 0; i < s.size(); i++)
-    {
-        if (map1.contains(s[i]))
-            map1[s[i]]++;
-        else
-            map1.insert({s[i], 1});
-    }
+    sort(s.begin(), s.end());
+    sort(t.begin(), t.end());
 
-    for (int j = 0; j < t.size(); j++)
-    {
-        if (!map1.contains(t[j]) || map1[t[j]] <= 0)
-            return false;
-        else
-            map1[t[j]]--;
-    }
-
-    return true;
+    return s == t;
 }
