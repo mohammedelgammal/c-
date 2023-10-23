fdd17ecf7489afcf3b22f0354beea5cf1aa2ba16
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-10-23 03:54:13 +0300

    implementing_isIsomorphic_problem_solution_using_positionning

--- Solution.cpp
+++ Solution.cpp
@@ -5,35 +5,18 @@
 
 using namespace std;
 
-int getCharIndex(const char character)
-{
-    return character - 97;
-}
-
 bool Solution::isIsomorphic(string s, string t)
 {
-    unordered_map<char, char> map;
-    set<int> set;
-
     if (s.size() != t.size())
         return false;
 
-    for (int i = 0; i < s.size(); i++)
+    int m1[256] = {0}, m2[256] = {0}, n = s.size();
+    for (int i = 0; i < n; ++i)
     {
-        if (map.contains(s[i]))
-        {
-            if (map.find(s[i])->second != t[i])
-                return false;
-        }
-        else
-        {
-            if (set.contains(t[i]))
-                return false;
-
-            set.insert(t[i]);
-            map.insert({s[i], t[i]});
-        }
+        if (m1[s[i]] != m2[t[i]])
+            return false;
+        m1[s[i]] = i + 1;
+        m2[t[i]] = i + 1;
     }
-
     return true;
 }
