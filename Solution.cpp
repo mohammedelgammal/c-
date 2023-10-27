4fb8a9aa9b8413b60b11c17357a0bb470a11f139
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-10-27 04:20:36 +0200

    implementing_isSameTree_problem_solution

--- Solution.cpp
+++ Solution.cpp
@@ -4,63 +4,9 @@
 
 using namespace std;
 
-ListNode *getMin(ListNode *list1, ListNode *list2)
+bool Solution::isSameTree(TreeNode *p, TreeNode *q)
 {
-    return list1->val < list2->val ? list1 : list2;
-}
-
-void updateLists(ListNode *minNode, ListNode *&list1, ListNode *&list2)
-{
-    if (minNode == list1)
-        list1 = list1->next;
-    else
-        list2 = list2->next;
-}
-
-ListNode *Solution::mergeTwoLists(ListNode *list1, ListNode *list2)
-{
-    if (list1 == NULL)
-        return list2;
-
-    if (list2 == NULL)
-        return list1;
-
-    ListNode *ptr = list1;
-
-    if (list1->val > list2->val)
-    {
-        ptr = list2;
-        list2 = list2->next;
-    }
-    else
-    {
-        list1 = list1->next;
-    }
-
-    
-    ListNode *curr = ptr;
-
-    // till one of the list doesn't reaches NULL
-    while (list1 && list2)
-    {
-        if (list1->val < list2->val)
-        {
-            curr->next = list1;
-            list1 = list1->next;
-        }
-        else
-        {
-            curr->next = list2;
-            list2 = list2->next;
-        }
-        curr = curr->next;
-    }
-
-    // adding remaining elements of bigger list.
-    if (!list1)
-        curr->next = list2;
-    else
-        curr->next = list1;
-
-    return ptr;
+    if (!p || !q)
+        return p == q;
+    return p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
 }
--- main.cpp
+++ main.cpp
@@ -1,54 +1,13 @@
 // Libraries
 #include <iostream>
-#include <sstream>
 
 #include "Solution"
 
 using namespace std;
 
-void deleteLinkedList(ListNode *head)
-{
-    while (head)
-    {
-        ListNode *current = head;
-        head = head->next;
-        delete current;
-    }
-}
-
-void traverseLinkedList(ListNode *head)
-{
-    ListNode *current = head;
-    while (current != nullptr)
-    {
-        // Process the current node (e.g., print its value)
-        cout << current->val << " ";
-        current = current->next; // Move to the next node
-    }
-    cout << endl;
-}
-
 int main()
 {
     Solution solution;
-
-    ListNode node1{1};
-    ListNode node2{2};
-    ListNode node3{4};
-
-    ListNode *list1 = &node1;
-    list1->next = &node2;
-    list1->next->next = &node3;
-
-    ListNode node4{1};
-    ListNode node5{3};
-    ListNode node6{4};
-
-    ListNode *list2 = &node4;
-    list2->next = &node5;
-    list2->next->next = &node6;
-
-    traverseLinkedList(solution.mergeTwoLists(list1, list2));
 
     return 0;
 }
