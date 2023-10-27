2b7e623ee7e179d53e0fc4317f26880e4cd0ae74
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-10-27 03:52:35 +0200

    implementing_mergeTwoLists_problem_solution

--- Solution.cpp
+++ Solution.cpp
@@ -1,23 +1,66 @@
 #include <iostream>
-#include <stack>
-#include <set>
 
 #include "Solution"
 
 using namespace std;
 
-bool Solution::hasCycle(ListNode *head)
+ListNode *getMin(ListNode *list1, ListNode *list2)
 {
-    ListNode *tortoise = head, *hare = head;
+    return list1->val < list2->val ? list1 : list2;
+}
 
-    while (hare != nullptr && hare->next)
+void updateLists(ListNode *minNode, ListNode *&list1, ListNode *&list2)
+{
+    if (minNode == list1)
+        list1 = list1->next;
+    else
+        list2 = list2->next;
+}
+
+ListNode *Solution::mergeTwoLists(ListNode *list1, ListNode *list2)
+{
+    if (list1 == NULL)
+        return list2;
+
+    if (list2 == NULL)
+        return list1;
+
+    ListNode *ptr = list1;
+
+    if (list1->val > list2->val)
     {
-        tortoise = tortoise->next;
-        hare = hare->next->next;
-
-        if (hare == tortoise)
-            return true;
+        ptr = list2;
+        list2 = list2->next;
+    }
+    else
+    {
+        list1 = list1->next;
     }
 
-    return false;
+    
+    ListNode *curr = ptr;
+
+    // till one of the list doesn't reaches NULL
+    while (list1 && list2)
+    {
+        if (list1->val < list2->val)
+        {
+            curr->next = list1;
+            list1 = list1->next;
+        }
+        else
+        {
+            curr->next = list2;
+            list2 = list2->next;
+        }
+        curr = curr->next;
+    }
+
+    // adding remaining elements of bigger list.
+    if (!list1)
+        curr->next = list2;
+    else
+        curr->next = list1;
+
+    return ptr;
 }
