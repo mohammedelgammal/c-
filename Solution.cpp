05b0e392bef96dd4f2faa80fa2ef6545696e97c7
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-11-05 22:01:24 +0200

    implementing_rotate_problem_solution_reverse_std

--- Solution.cpp
+++ Solution.cpp
@@ -5,23 +5,13 @@
 
 using namespace std;
 
-void reverse(vector<int> &nums, int left, int right)
-{
-    while (left < right)
-    {
-        int temp = nums[left];
-        nums[left++] = nums[right];
-        nums[right--] = temp;
-    }
-}
-
 void Solution::rotate(vector<int> &nums, int k)
 {
     int size = nums.size();
 
     k %= size;
 
-    reverse(nums, 0, size - 1);
-    reverse(nums, 0, k - 1);
-    reverse(nums, k, size - 1);
+    reverse(nums.begin(), nums.end());
+    reverse(nums.begin(), nums.begin() + k);
+    reverse(nums.begin() + k, nums.end());
 }
