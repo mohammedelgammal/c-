f26fedf9c64aecf4afe2cefc71052280ea6edfd7
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-11-06 01:32:29 +0200

    implementing_fib_problem_solution_exponential_time_constant_space

--- Solution.cpp
+++ Solution.cpp
@@ -10,14 +10,5 @@ int Solution:: fib(int n)
     if (n < 2)
         return n;
 
-    int first = 1, second = 0;
-
-    for (int i = 0; i < n - 1; i++)
-    {
-        int temp = first;
-        first += second;
-        second = temp;
-    }
-
-    return first;
+    return fib(n - 2) + fib(n - 1);
 }
