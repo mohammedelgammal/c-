2698b5bba7ae2a0cd31a26846d108b91d3bc638b
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-11-05 21:50:15 +0200

    implementing_maxProfit_problem_solution_optimized

--- Solution.cpp
+++ Solution.cpp
@@ -7,29 +7,18 @@ using namespace std;
 
 int Solution::maxProfit(vector<int> &prices)
 {
-    int left = 0, right = 1;
-    vector<int> buy, sell;
+    int left = 0,
+        right = 1,
+        size = prices.size(),
+        maxProfit = 0;
 
-    while (right < prices.size())
+    while (right < size)
     {
-        if (prices[left] < prices[right])
-        {
-            buy.push_back(prices[left]);
-            sell.push_back(prices[right]);
-        }
-        left++;
-        right++;
+        int todayPrice = prices[left++],
+            nextPrice = prices[right++];
+
+        if (todayPrice < nextPrice)
+            maxProfit += (nextPrice - todayPrice);
     }
-
-    int maxProfit = sell[sell.size() - 1] - buy[0];
-    int j = 0;
-    int profit = 0;
-
-    while (j < max(sell.size(), buy.size()))
-    {
-        profit += (sell[j] - buy[j]);
-        j++;
-    }
-
-    return max(profit, maxProfit);
+    return maxProfit;
 }
