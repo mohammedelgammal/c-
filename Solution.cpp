3299079a14483dfe6a39e0f76c3315deb8dfb818
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-11-11 21:10:50 +0200

    implementing_productExceptSelf_problem_solution_optimized_constant_space_linear_time

--- Solution.cpp
+++ Solution.cpp
@@ -8,34 +8,20 @@ using namespace std;
 vector<int> Solution::productExceptSelf(vector<int> &nums)
 {
     int size = nums.size();
-    vector<int> prefix(size, 0),
-        postfix(size, 0),
-        ans(size, 0);
+    vector<int> ans(size, 1);
+    int prefix = 1;
+    int postfix = 1;
 
     for (int i = 0; i < size; i++)
     {
-        int prev = 1;
-        if (i > 0)
-            prev = prefix[i - 1];
-        prefix[i] = nums[i] * prev;
+        ans[i] *= prefix;
+        prefix *= nums[i];
     }
 
     for (int j = size - 1; j >= 0; j--)
     {
-        int prev = 1;
-        if (j < size - 1)
-            prev = postfix[j + 1];
-        postfix[j] = nums[j] * prev;
-    }
-
-    for (int k = 0; k < size; k++)
-    {
-        int postPrev = 1, prePrev = 1;
-        if (k > 0)
-            prePrev = prefix[k - 1];
-        if (k < size - 1)
-            postPrev = postfix[k + 1];
-        ans[k] = postPrev * prePrev;
+        ans[j] *= postfix;
+        postfix *= nums[j];
     }
 
     return ans;
