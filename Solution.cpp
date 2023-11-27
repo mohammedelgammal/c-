530991beb334d77b81451c62b32ce1aa97ca8216
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-11-27 23:14:30 +0200

    implementing_setZeroes_problem_solution_tcO(m * n)_scO(1)

--- Solution.cpp
+++ Solution.cpp
@@ -6,20 +6,27 @@
 void Solution::setZeroes(vector<vector<int>> &matrix)
 {
     int rows = matrix.size(),
-        cols = matrix[0].size();
-    set<int> rs, cs;
-
+        cols = matrix[0].size(),
+        tl = 1;
     for (int r = 0; r < rows; r++)
         for (int c = 0; c < cols; c++)
             if (matrix[r][c] == 0)
             {
-                rs.insert(r);
-                cs.insert(c);
+                r > 0 ? matrix[r][0] = 0 : tl = 0;
+                matrix[0][c] = 0;
             }
-    for (int r : rs)
+    for (int r = 1; r < rows; r++)
+        if (matrix[r][0] == 0)
+            for (int c = 1; c < cols; c++)
+                matrix[r][c] = 0;
+    for (int c = 1; c < cols; c++)
+        if (matrix[0][c] == 0)
+            for (int r = 1; r < rows; r++)
+                matrix[r][c] = 0;
+    if (matrix[0][0] == 0)
+        for (int r = 1; r < rows; r++)
+            matrix[r][0] = 0;
+    if (tl == 0)
         for (int c = 0; c < cols; c++)
-            matrix[r][c] = 0;
-    for (int c : cs)
-        for (int r = 0; r < rows; r++)
-            matrix[r][c] = 0;
+            matrix[0][c] = 0;
 }
