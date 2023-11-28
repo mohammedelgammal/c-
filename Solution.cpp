25e26f9c595531c89abc66d35cb159949f69c03a
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-11-29 00:00:28 +0200

    implementing_gameOfLife_problem_solution_tcO(m * n)_scO(1)

--- Solution.cpp
+++ Solution.cpp
@@ -6,7 +6,6 @@ void Solution::gameOfLife(vector<vector<int>> &board)
 {
     int rows = board.size(),
         cols = board[0].size();
-    vector<vector<int>> ans = board;
 
     for (int r = 0; r < rows; r++)
         for (int c = 0; c < cols; c++)
@@ -15,16 +14,23 @@ void Solution::gameOfLife(vector<vector<int>> &board)
             for (int i = r - 1; i <= r + 1; i++)
                 for (int j = c - 1; j <= c + 1; j++)
                 {
-                    if (i < 0 || i >= rows || j < 0 || j >= cols ||
+                    if (i < 0 || j < 0 || i >= rows || j >= cols ||
                         (i == r && j == c))
                         continue;
-                    if (board[i][j] == 1)
+                    if (board[i][j] == 1 || board[i][j] == 3)
                         ln++;
                 }
             if (board[r][c] == 0 && ln == 3)
-                ans[r][c] = 1;
+                board[r][c] = 2;
             if (board[r][c] == 1 && (ln < 2 || ln > 3))
-                ans[r][c] = 0;
+                board[r][c] = 3;
         }
-    board = ans;
+    for (int r = 0; r < rows; r++)
+        for (int c = 0; c < cols; c++)
+        {
+            if (board[r][c] == 3)
+                board[r][c] = 0;
+            if (board[r][c] == 2)
+                board[r][c] = 1;
+        }
 }
