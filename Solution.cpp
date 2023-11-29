8e2d9b26d51ee5f66327749a99f2ef1ef4bd992b
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-11-29 13:01:29 +0200

    implementing_numberOfBeams_problem_solution

--- Solution.cpp
+++ Solution.cpp
@@ -2,35 +2,21 @@
 
 #include "Solution"
 
-void Solution::gameOfLife(vector<vector<int>> &board)
+int Solution::numberOfBeams(vector<string> &bank)
 {
-    int rows = board.size(),
-        cols = board[0].size();
+    int prev = 0, ans = 0;
 
-    for (int r = 0; r < rows; r++)
-        for (int c = 0; c < cols; c++)
+    for (string row : bank)
+    {
+        int count = 0;
+        for (char ch : row)
+            if (ch == '1')
+                count++;
+        if (count)
         {
-            int ln = 0;
-            for (int i = r - 1; i <= r + 1; i++)
-                for (int j = c - 1; j <= c + 1; j++)
-                {
-                    if (i < 0 || j < 0 || i >= rows || j >= cols ||
-                        (i == r && j == c))
-                        continue;
-                    if (board[i][j] == 1 || board[i][j] == 3)
-                        ln++;
-                }
-            if (board[r][c] == 0 && ln == 3)
-                board[r][c] = 2;
-            if (board[r][c] == 1 && (ln < 2 || ln > 3))
-                board[r][c] = 3;
+            ans += prev * count;
+            prev = count;
         }
-    for (int r = 0; r < rows; r++)
-        for (int c = 0; c < cols; c++)
-        {
-            if (board[r][c] == 3)
-                board[r][c] = 0;
-            if (board[r][c] == 2)
-                board[r][c] = 1;
-        }
+    }
+    return ans;
 }
--- main.cpp
+++ main.cpp
@@ -10,21 +10,7 @@ int main()
 {
     Solution solution;
 
-    vector<vector<int>> mat{{1, 1}, {1, 0}};
+    vector<string> bank{"000", "111", "000"};
 
-    solution.gameOfLife(mat);
-
-    cout << '[';
-
-    for (vector<int> vec : mat)
-    {
-        cout << '[';
-        for (int el : vec)
-            cout << el << ',';
-        cout << ']';
-    }
-
-    cout << ']';
-
-    return 0;
+    cout << solution.numberOfBeams(bank);
 }
