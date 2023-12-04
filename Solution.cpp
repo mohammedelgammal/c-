0209b2c4625b9457d6ac9d27e0110e21a321d797
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-12-04 23:29:30 +0200

    implementing_findAnagrams_problem_solution_tcO(n)_scO(1)

--- Solution.cpp
+++ Solution.cpp
@@ -1,27 +1,33 @@
 #include <iostream>
-
+#include <algorithm>
 #include "Solution"
 
 vector<int> Solution::findAnagrams(string s, string p)
 {
+    const int sSize = s.size(), pSize = p.size(), COUNT = 26;
+    if (sSize < pSize)
+        return {};
     vector<int> ans;
-    int left = 0, right = p.size() - 1;
-
-    while (right < s.size())
+    int pCount[COUNT] = {0},
+        sCount[COUNT] = {0},
+        left = 0,
+        right = pSize - 1;
+    for (int i = 0; i < pSize; i++)
+    {
+        pCount[p[i] - 'a']++;
+        sCount[s[i] - 'a']++;
+    }
+    while (right < sSize)
     {
         bool isAnagram = true;
-        int count[26] = {0};
-        for (int i = left; i < right + 1; i++)
-            count[s[i] - 'a']++;
-        for (char c : p)
-            count[c - 'a']--;
-        for (int n : count)
-            if (n)
+        for (int i = 0; i < COUNT; i++)
+            if (sCount[i] != pCount[i])
                 isAnagram = false;
         if (isAnagram)
             ans.push_back(left);
-        left++;
-        right++;
+        if (++right < sSize)
+            sCount[s[right] - 'a']++;
+        sCount[s[left++] - 'a']--;
     }
     return ans;
 }
