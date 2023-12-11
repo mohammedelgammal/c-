b7351c1009e9b567dc3e40cd56502e23a5e08981
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2023-12-12 01:05:59 +0200

    implementing_findSpecialInteger_streak_problem_solution_tcO(log n)_sc_O(1)

--- Solution.cpp
+++ Solution.cpp
@@ -2,15 +2,28 @@
 #include <unordered_set>
 #include "Solution"
 
+int findLeftMostIdx(const vector<int> &arr, const int n)
+{
+    int l = 0, r = arr.size() - 1;
+
+    while (l <= r)
+    {
+        int m = l + (r - l) / 2;
+        if (arr[m] >= n)
+            r = m - 1;
+        else
+            l = m + 1;
+    }
+
+    return l;
+}
+
 int Solution::findSpecialInteger(vector<int> &arr)
 {
+    // using binary search
     int size = arr.size();
-    unordered_map<int, int> map;
-
-    for (int i = 0; i < size; i++)
-        map[arr[i]]++;
-    for (auto it : map)
-        if (it.second > size / 4)
-            return it.first;
+    for (int idx : {size / 4, size / 2, size * 3 / 4})
+        if (arr[findLeftMostIdx(arr, arr[idx]) + size / 4] == arr[idx])
+            return arr[idx];
     return -1;
 }
