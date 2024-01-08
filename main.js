1d1213fb1288026991fd00542d0baceb606e8e4e
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2024-01-09 01:57:46 +0200

    implementing_iterator_to_objects_using_generator

--- main.js
+++ main.js
@@ -1,19 +1,11 @@
-Object.prototype[Symbol.iterator] = function () {
-  let index = 0;
-  const entries = Object.entries(this);
-  return {
-    next() {
-      return index < entries.length
-        ? { value: [entries[index][0], entries[index++][1]], done: false }
-        : { done: true };
-    },
-  };
+Object.prototype[Symbol.iterator] = function* () {
+  for (let key in this) yield [key, this[key]];
 };
 
-const obj = {
+const person = {
   name: "ali",
   age: 28,
-  country: "syria",
+  country: "Syria",
 };
 
-for (let [key, value] of obj) console.log(key, value);
+for (let [key, val] of person) console.log(key, val);
