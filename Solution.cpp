3de7bc6fa57750d5d17fe895251e897d18d6a514
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2024-01-09 04:48:51 +0200

    implementing_climbStairs_problem_solution_using_top_bottom_dp_recursion_using_memoization_tcO(n)_scO(n)

--- Solution.cpp
+++ Solution.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 #include "Solution"
 
-int Solution::climbStairs(int n)
+int climb(int n, unordered_map<int, int> &memo)
 {
     if (n <= 1)
         return 1;
-    return climbStairs(n - 1) + climbStairs(n - 2);
+
+    if (memo.contains(n))
+        return memo[n];
+
+    memo[n] = climb(n - 1, memo) + climb(n - 2, memo);
+    return memo[n];
+}
+
+int Solution::climbStairs(int n)
+{
+    unordered_map<int, int> memo;
+    return climb(n, memo);
 }
