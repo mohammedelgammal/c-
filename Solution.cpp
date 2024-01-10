f92a1962674948ef4bcf845dfc57dfa446b0fad0
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2024-01-10 14:18:16 +0200

    implementing_tribonacci_problem_dp_bottom_up_tabulation_solution_tcO(n)_scO(n)

--- Solution.cpp
+++ Solution.cpp
@@ -1,20 +1,11 @@
 #include <iostream>
 #include "Solution"
 
-int trib(int n, unordered_map<int, int> &memo)
-{
-    if (n < 2)
-        return n;
-    if (n == 2)
-        return 1;
-    if (memo.contains(n))
-        return memo[n];
-    memo[n] = trib(n - 3, memo) + trib(n - 2, memo) + trib(n - 1, memo);
-    return memo[n];
-}
-
 int Solution::tribonacci(int n)
 {
-    unordered_map<int, int> memo;
-    return trib(n, memo);
+    vector<int> memo(n + 1, 1);
+    memo.front() = 0;
+    for (int i = 3; i < memo.size(); i++)
+        memo[i] = memo[i - 1] + memo[i - 2] + memo[i - 3];
+    return memo.back();
 }
