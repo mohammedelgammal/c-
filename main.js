9ef396508cdc2fd9cbb9c443f8e4c8ffb9457b25
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2024-01-20 04:55:32 +0200

    implementing_isInt_function

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-// random color generator
-(function () {
-  console.log(`#${Math.random().toString(16).substring(2, 8)}`);
-})();
+// determine if the number is an integer
+const isInt = function (n) {
+  return !isNaN(n) && parseInt(n) === n;
+};
+
+console.log(isInt(3));
+console.log(isInt(3.5));
+console.log(isInt(9));
