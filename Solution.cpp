b05ef795908c8c33e100ebd507b6d679180a3ebc
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2024-01-21 22:56:23 +0200

    implementing_minCostClimbingStairs_problem_solution_top_down_approach_using_memoization_dp_tcO(n)_scO(n)

--- Solution.cpp
+++ Solution.cpp
@@ -3,14 +3,18 @@
 
 #include "Solution"
 
-int climb(int step, vector<int> &cost)
+int climb(int step, vector<int> &cost, unordered_map<int, int> &memo)
 {
     if (step >= cost.size())
         return 0;
-    return cost[step] + min(climb(step + 1, cost), climb(step + 2, cost));
+    if (memo.contains(step))
+        return memo[step];
+    memo[step] = cost[step] + min(climb(step + 1, cost, memo), climb(step + 2, cost, memo));
+    return memo[step];
 }
 
 int Solution::minCostClimbingStairs(vector<int> &cost)
 {
-    return min(climb(0, cost), climb(1, cost));
+    unordered_map<int, int> memo;
+    return min(climb(0, cost, memo), climb(1, cost, memo));
 }
