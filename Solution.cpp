99057c087e161d0d5ae8d62affd492e5f4aa0b7d
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2024-01-24 22:39:15 +0200

    implementing_robII_problem_solution_recursion_tcO(2^n)_scO(n)

--- Solution.cpp
+++ Solution.cpp
@@ -3,16 +3,15 @@
 
 #include "Solution"
 
+int robIt(int index, vector<int> &nums, bool isStart)
+{
+    if (index >= nums.size() || (index + 1 == nums.size() && isStart))
+        return 0;
+    return max(nums[index] + robIt(index + 2, nums, true),
+               robIt(index + 1, nums, false));
+}
+
 int Solution::rob(vector<int> &nums)
 {
-    int size = nums.size(),
-        choiceOne = 0,
-        choiceTwo = nums[size - 1];
-    for (int i = size - 2; i >= 0; i--)
-    {
-        int temp = choiceTwo;
-        choiceTwo = max(nums[i] + choiceOne, choiceTwo);
-        choiceOne = temp;
-    }
-    return choiceTwo;
+    return robIt(0, nums, true);
 }
--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main()
 {
     Solution solution;
 
-    vector<int> cost = {2, 7, 9, 3, 1};
+    vector<int> cost = {1, 2, 3, 1};
 
     cout << solution.rob(cost);
 
