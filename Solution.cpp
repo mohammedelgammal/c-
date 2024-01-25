2310bcea25850a6431afdcc2bcef9c42287602b1
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2024-01-25 21:27:31 +0200

    implementing_robII_problem_solution_dp_bottom_up_tabulation_tcO(n)_scO(n)

--- Solution.cpp
+++ Solution.cpp
@@ -3,23 +3,20 @@
 
 #include "Solution"
 
-int robIt(int l, int r, vector<int> &nums, unordered_map<int, int> &memo)
+int robIt(int l, int r, vector<int> &nums)
 {
-    if (l > r)
-        return 0;
-    if (memo.contains(l))
-        return memo[l];
-    memo[l] = max(nums[l] + robIt(l + 2, r, nums, memo),
-                  robIt(l + 1, r, nums, memo));
-    return memo[l];
+    int size = nums.size();
+    vector<int> memo(size);
+    memo[size - 2] = nums[r];
+    for (int i = r - 1, k = size - 3; i >= l; i--, k--)
+        memo[k] = max(nums[i] + memo[k + 2], memo[k + 1]);
+    return memo.front();
 }
 
 int Solution::rob(vector<int> &nums)
 {
     if (nums.size() == 1)
         return nums.front();
-    int size = nums.size();
-    unordered_map<int, int> memoOne, memoTwo;
-    return max(robIt(0, size - 2, nums, memoOne),
-               robIt(1, size - 1, nums, memoTwo));
+    return max(robIt(0, nums.size() - 2, nums),
+               robIt(1, nums.size() - 1, nums));
 }
