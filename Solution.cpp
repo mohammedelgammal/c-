f0db89504a5bed5f6826348fb93afee38ae72654
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2024-01-25 21:25:53 +0200

    implementing_robII_problem_solution_dp_top_down_memoization_tcO(n)_scO(n)

--- Solution.cpp
+++ Solution.cpp
@@ -3,11 +3,15 @@
 
 #include "Solution"
 
-int robIt(int l, int r, vector<int> &nums)
+int robIt(int l, int r, vector<int> &nums, unordered_map<int, int> &memo)
 {
     if (l > r)
         return 0;
-    return max(nums[l] + robIt(l + 2, r, nums), robIt(l + 1, r, nums));
+    if (memo.contains(l))
+        return memo[l];
+    memo[l] = max(nums[l] + robIt(l + 2, r, nums, memo),
+                  robIt(l + 1, r, nums, memo));
+    return memo[l];
 }
 
 int Solution::rob(vector<int> &nums)
@@ -15,5 +19,7 @@ int Solution::rob(vector<int> &nums)
     if (nums.size() == 1)
         return nums.front();
     int size = nums.size();
-    return max(robIt(0, size - 2, nums), robIt(1, size - 1, nums));
+    unordered_map<int, int> memoOne, memoTwo;
+    return max(robIt(0, size - 2, nums, memoOne),
+               robIt(1, size - 1, nums, memoTwo));
 }
