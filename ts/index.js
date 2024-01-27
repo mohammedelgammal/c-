86669ce8042e4d8f0aa8d202ac4cec04368dd618
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2024-01-27 21:37:17 +0200

    basic_typescript_&_types_type_annotations

--- ts/index.js
+++ ts/index.js
@@ -1,19 +1,17 @@
-"use strict";
-var _a;
 // explicit type annotation
-let num = 0;
+var num = 0;
 // implicit type => any
-let x;
+var x;
 // explicit type any
-let doc = "mine";
+var doc = "mine";
 doc = 21;
 // array of any
-let arr = [1, true, "fa"];
+var arr = [1, true, "fa"];
 // array of single type
-let list = [1, 2, 3];
+var list = [1, 2, 3];
 // tuples can be two typically or more
-let tuple = [1, true];
-let tup = [1, true, "lir", false];
+var tuple = [1, true];
+var tup = [1, true, "lir", false];
 // enums - just like others
 var Size;
 (function (Size) {
@@ -21,40 +19,32 @@ var Size;
     Size[Size["Medium"] = 2] = "Medium";
     Size[Size["Large"] = 3] = "Large";
 })(Size || (Size = {}));
-const mySize = Size.Large; // 3
+var mySize = Size.Large; // 3
 // functions and optional args
 function print(age) {
-    let x = 10;
+    var x = 10;
     if (age < 18)
         return -1;
     return 1 * x;
 }
 // objects & shapes & methods & readonly
-let obj = {
+var obj = {
     name: "more",
     file: false,
-    retire: (date = new Date()) => console.log(date),
+    retire: function (date) {
+        if (date === void 0) { date = new Date(); }
+        return console.log(date);
+    },
 };
-// type unoin => one of 'em
+// type unoin
 function weigh(weight) {
     if (typeof weight === "string")
         return parseInt(weight);
     else
         return weight;
 }
-const resize = {
-    resize: () => console.log("resize"),
-    drag: () => console.log("drag"),
-};
-const len = 50;
-// optional member accessing operator
-function getEmployee(emplyee) {
-    return emplyee;
-}
 console.log(mySize);
 console.log(print(10));
 obj.retire(new Date());
 console.log(weigh("10kg"));
-console.log(weigh(12));
-console.log((_a = getEmployee()) === null || _a === void 0 ? void 0 : _a.name); // undefined
-//# sourceMappingURL=index.js.map
+console.log(weigh(10));
--- ts/index.ts
+++ ts/index.ts
@@ -1,3 +1,88 @@
+// explicit type annotation
 let num: number = 0;
+// implicit type => any
+let x;
+// explicit type any
+let doc: any = "mine";
+doc = 21;
+// array of any
+let arr: any[] = [1, true, "fa"];
+// array of single type
+let list: number[] = [1, 2, 3];
+// tuples can be two typically or more
+let tuple: [number, boolean] = [1, true];
+let tup: [number, boolean, string, boolean] = [1, true, "lir", false];
+// enums - just like others
+enum Size {
+  Small = 1,
+  Medium,
+  Large,
+}
+const mySize: Size = Size.Large; // 3
+// functions and optional args
+function print(age: number): number {
+  let x: number = 10;
+  if (age < 18) return -1;
+  return 1 * x;
+}
+// type alias
+type Employee = {
+  name: string;
+  age?: number;
+  readonly file: boolean;
+  retire: (date: Date) => void;
+};
+// objects & shapes & methods & readonly
+let obj: Employee = {
+  name: "more",
+  file: false,
+  retire: (date: Date = new Date()) => console.log(date),
+};
+// type unoin => one of 'em
+function weigh(weight: number | string): number | null {
+  if (typeof weight === "string") return parseInt(weight);
+  else return weight;
+}
+// type intersection => combine two types &
+type Draggable = {
+  drag: () => void;
+};
+type Resizeable = {
+  resize: () => void;
+};
+type ResizeableDragabble = Draggable & Resizeable;
+const resize: ResizeableDragabble = {
+  resize: () => console.log("resize"),
+  drag: () => console.log("drag"),
+};
+// literal type
+type Metric = "cm" | "inch" | 50;
+const len: Metric = 50;
+// optional member accessing operator
+function getEmployee(emplyee: Employee | null): Employee | null | undefined {
+  return emplyee ?? undefined;
+}
+// Nullish coalescing operator ?? VVVVV>>>>>IIIIIII
+// instead of: !employee ? somevalue : employee instead=> employee ?? 30
+// type assersion
+let commonWeight: number = weigh("10kg") as number;
+let otherWeight: number = <number>weigh(10);
+// unknown type
+function print(object: unknown): void {
+  if (typeof object === "string") console.log(object.length);
+  if (typeof object === "number") console.log(object.toString().length);
+  if (object instanceof Boolean) console.log(object);
+  return;
+}
+// never type
+function thrower(message: string): never {
+  throw new Error(message);
+}
+// thrower("an error ocurred!"); // code below this line is unreachable using config file
 
-console.log(num);
+console.log(mySize);
+console.log(print(10));
+obj.retire(new Date());
+console.log(weigh("10kg"));
+console.log(weigh(12));
+console.log(getEmployee(null)?.name); // undefined
