97d30abc57b4013b85e22b209556153c48cfed4c
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2024-01-30 01:49:05 +0200

    implementing_countSubstrings_problem_solution_using_helper_function

--- Solution.cpp
+++ Solution.cpp
@@ -3,18 +3,19 @@
 
 #include "Solution"
 
+void countIt(int l, int r, int &ans, string &s)
+{
+    while (l >= 0 && r < s.size() && s.at(l) == s.at(r))
+        (ans++, l--, r++);
+}
+
 int Solution::countSubstrings(string s)
 {
     int ans = 0;
     for (int i = 0; i < s.size(); i++)
     {
-        int l = i, r = i;
-        while (l >= 0 && r < s.size() && s.at(l) == s.at(r))
-            (ans++, l--, r++);
-        l = i - 1;
-        r = i;
-        while (l >= 0 && r < s.size() && s.at(l) == s.at(r))
-            (ans++, l--, r++);
+        countIt(i, i, ans, s);
+        countIt(i - 1, i, ans, s);
     }
     return ans;
 }
