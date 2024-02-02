9e2a990ada030fb85eec9d5706106aa5b8ed8dd0
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2024-02-02 04:35:53 +0200

    implementing_form_using_useForm_&_handling_errors

--- reacts/src/Components/Form.tsx
+++ reacts/src/Components/Form.tsx
@@ -1,36 +1,67 @@
 import { FieldValues, useForm } from "react-hook-form";
+import { Person } from "../types";
 
 export default (): JSX.Element => {
-  const { register, handleSubmit } = useForm();
-  const nameField = register("name");
-  const ageField = register("age");
-  const submitHandler = (formValues: FieldValues): void => {
-    console.log(formValues.name);
+  const {
+    register,
+    handleSubmit,
+    formState: {
+      errors: { name: nameError, age: ageError },
+    },
+  } = useForm<Person>();
+  const submitHandler = (fieldValues: FieldValues): void => {
+    console.log(fieldValues);
   };
+  const nameField = register("name", {
+    required: "Name is a required field",
+    minLength: {
+      value: 2,
+      message: "Name can be at least 2 characters",
+    },
+    pattern: {
+      value: /^[A-Za-z]+$/i,
+      message: "Invalid name input",
+    },
+  });
+  const ageField = register("age", {
+    required: "Age is a required field",
+    min: {
+      value: 18,
+      message: "You should be at least 18 years old",
+    },
+    max: {
+      value: 99,
+      message: "Age must be max 99 years",
+    },
+  });
   return (
     <div className="m-3">
-      <form onSubmit={handleSubmit(submitHandler)}>
+      <form
+        onSubmit={handleSubmit((fieldValues) => submitHandler(fieldValues))}
+      >
         <div className="mb-3">
           <label className="form-label" htmlFor="name">
             Name
           </label>
           <input
+            {...nameField}
             type="text"
             className="form-control"
             id="name"
-            {...nameField}
           />
+          <p className="text-danger">{nameError?.message}</p>
         </div>
         <div className="mb-3">
           <label className="form-label" htmlFor="age">
             Age
           </label>
           <input
+            {...ageField}
             type="number"
             className="form-control"
             id="age"
-            {...ageField}
           />
+          <p className="text-danger">{ageError?.message}</p>
         </div>
         <button className="btn btn-primary" type="submit">
           Submit
