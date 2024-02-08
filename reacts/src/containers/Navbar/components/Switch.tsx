dff6f315ab3b4ea2d77f1ed3c27425946ad14df5
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2024-02-08 02:05:29 +0200

    implementing_color_switch

--- reacts/src/containers/Navbar/components/Switch.tsx
+++ reacts/src/containers/Navbar/components/Switch.tsx
@@ -1,15 +1,18 @@
 import { Box, Switch } from "@chakra-ui/react";
+import { useColorMode } from "@chakra-ui/react";
+import { SunIcon, MoonIcon } from "@chakra-ui/icons";
 
 export default (): JSX.Element => {
-  const handleSwitch = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(e.target.checked);
-  };
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
     <Box flex="initial" display="flex" alignItems="center" gap="10px">
-      <Switch id="switch" onChange={(e) => handleSwitch(e)} />
-      <label style={{ whiteSpace: "nowrap" }} htmlFor="switch">
-        Dark Mode
-      </label>
+      <Switch
+        colorScheme="green"
+        isChecked={colorMode === "light"}
+        onChange={toggleColorMode}
+      />
+      {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
     </Box>
   );
 };
