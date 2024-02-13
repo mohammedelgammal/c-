94558c62e9383cfd596d20de0b2772c7c870e8cf
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2024-02-13 02:05:52 +0200

    Revert "implementing_useGames_hook_delete_useGenres_useGames_old_refactored"
    This reverts commit fc41de9b5ab42be75cc2d33be308d8c947565e05.

--- reacts/src/App.tsx
+++ reacts/src/App.tsx
@@ -1,10 +1,34 @@
-import { Flex, Stack } from "@chakra-ui/react";
-import Posts from "./components/Posts";
-import PostsForm from "./components/PostsForm";
-import Todos from "./components/Todos";
-import TodosForm from "./components/TodosForm";
+import { useState } from "react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import { Navbar, Aside, Main } from "./containers";
+import useGenre from "./services/useGenre";
+import { Filters } from "./types";
+import useGames from "./services/useGames";
 
 export default (): JSX.Element => {
+  const {
+    genres,
+    isLoading: isGeneresLoading,
+    error: genresError,
+  } = useGenre();
+  const [filters, setFilters] = useState<Filters>({
+    genres: {
+      id: 0,
+      name: "",
+      slug: "",
+      games: [],
+      image_background: "",
+    },
+    platforms: "",
+    ordering: "",
+    search: "",
+  });
+  const {
+    games,
+    isLoading: isGamesLoading,
+    error: gamesError,
+  } = useGames(filters);
+
   return (
     <Flex gap={20}>
       <Stack spacing={10} flex={1}>
---
+++ reacts/src/containers/Main/Main.tsx
@@ -0,0 +1,43 @@
+import { Stack, Text, SimpleGrid } from "@chakra-ui/react";
+import { MainProps } from "../../types";
+import Select from "./components/Select";
+import Game from "./components/Game";
+import LoadingGames from "./components/LoadingGames";
+import ResponseHandler from "../../common/ResponseHandler";
+
+export default ({
+  categoryTitle,
+  games,
+  isLoading,
+  error,
+  setFilters,
+}: MainProps): JSX.Element => {
+  return (
+    <Stack>
+      <Text as="h2" fontSize="5xl" fontWeight="bold">
+        {categoryTitle || "Games"}
+      </Text>
+      <Select setFilters={setFilters} />
+      <ResponseHandler
+        error={error}
+        isLoading={isLoading}
+        loaderComponent={<LoadingGames length={15} />}
+        dataLength={games.length}
+      >
+        <SimpleGrid
+          marginTop="30px"
+          columns={{
+            sm: 1,
+            md: 2,
+            lg: 3,
+          }}
+          spacing={10}
+        >
+          {games.map((game, index) => (
+            <Game key={index} {...game} />
+          ))}
+        </SimpleGrid>
+      </ResponseHandler>
+    </Stack>
+  );
+};
