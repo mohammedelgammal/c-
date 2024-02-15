19d1ba9cd93d21bfb77742bf63f8829a03108625
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2024-02-15 22:39:13 +0200

    improving_Private_Routes_using_ternary

--- reacts/src/PrivateRoutes.tsx
+++ reacts/src/PrivateRoutes.tsx
@@ -3,6 +3,5 @@ import { Navigate, Outlet } from "react-router-dom";
 
 export default (): JSX.Element => {
   const { isLoggedIn } = useAuth();
-  if (!isLoggedIn) return <Navigate to="login" />;
-  return <Outlet />;
+  return !isLoggedIn ? <Navigate to="login" /> : <Outlet />;
 };
