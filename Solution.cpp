0a591fff5e80c9a8ea15065df1142ed822c50040
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2024-07-28 11:24:42 +0300

    implementing_maxProfit_problem_solution_tcO(N)_scO(1)

--- Solution.cpp
+++ Solution.cpp
@@ -3,24 +3,15 @@
 
 #include "Solution"
 
-double Solution::findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2)
+int Solution::maxProfit(vector<int> &prices)
 {
-    vector<int> &A = nums1, &B = nums2;
-    if(A.size() > B.size()) swap(A, B);
-    int total = A.size() + B.size(), 
-        half = total / 2,
-        l = 0, r = A.size() - 1;
-    while(true) {
-        int i = r >= 0 ? (l + r) / 2 : -1,
-            j = half - i - 2,
-            Al = i >= 0 ? A[i] : INT_MIN,
-            Ar = i + 1 < A.size() ? A[i + 1] : INT_MAX,
-            Bl = j >= 0 ? B[j] : INT_MIN, 
-            Br = j + 1 < B.size() ? B[j + 1] : INT_MAX;
-        if(Al <= Br && Bl <= Ar) 
-            return total % 2 ? min(Ar, Br) : (max(Al, Bl) + min(Ar, Br)) / 2.0;
-        else if(Al > Br) r = i - 1;
-        else l = i + 1; 
+    int l = 0, r = 1, ans = 0;
+    while(r < prices.size()) {
+        if(prices[r] > prices[l]) 
+            ans = max(prices[r] - prices[l], ans);
+        else
+            l = r;
+        r++;
     }
-    return -1;
+    return ans;
 }
