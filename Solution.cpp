5e3fe4ea8b3fc0d93334ce6bfedf84b476279355
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2024-09-08 04:21:20 +0300

    implementing_mergeTwoLists_problem_solution_tcO(m + n)_scO(1)

--- Solution.cpp
+++ Solution.cpp
@@ -3,16 +3,20 @@
 
 #include "Solution"
 
-ListNode *Solution::reverseList(ListNode *head)
+ListNode *Solution::mergeTwoLists(ListNode *list1, ListNode *list2)
 {
-    ListNode* newHead;
-    if(!head) return nullptr;
-    if(head->next) {
-        newHead = reverseList(head->next);
-        head->next->next = head;
-    } else {
-        newHead = head;
+    ListNode* head = new ListNode(), *tail = head;
+    while(list1 && list2) {
+        if(list1->val < list2->val) {
+            tail->next = list1;
+            list1 = list1->next;
+        } else {
+            tail->next = list2;
+            list2 = list2->next;
+        }
+        tail = tail->next;
     }
-    head->next = nullptr;
-    return newHead;
+    if(list1) tail->next = list1;
+    if(list2) tail->next = list2;
+    return head->next;
 }
