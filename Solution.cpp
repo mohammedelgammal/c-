c8fa7a5fbb91a101fd3d6453cdc83c75ab108b69
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2024-09-09 01:21:08 +0300

    implementing_reorderList_problem_solution_tcO(n)_scO(1)

--- Solution.cpp
+++ Solution.cpp
@@ -3,20 +3,29 @@
 
 #include "Solution"
 
-ListNode *Solution::mergeTwoLists(ListNode *list1, ListNode *list2)
+void Solution::reorderList(ListNode *head)
 {
-    ListNode* head = new ListNode(), *tail = head;
-    while(list1 && list2) {
-        if(list1->val < list2->val) {
-            tail->next = list1;
-            list1 = list1->next;
-        } else {
-            tail->next = list2;
-            list2 = list2->next;
-        }
-        tail = tail->next;
+    ListNode *slow = head, *fast = head->next;
+    while (fast && fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
     }
-    if(list1) tail->next = list1;
-    if(list2) tail->next = list2;
-    return head->next;
+    ListNode *prev = nullptr, *curr = slow->next, *next = nullptr;
+    slow->next = nullptr;
+    while (curr)
+    {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    ListNode *first = head, *second = prev, *next1 = nullptr, *next2 = nullptr;
+    while(second) {
+        next1 = first->next;
+        next2 = second->next;
+        first->next = second;
+        second->next = next1;
+        first = next1, second = next2;
+    }
 }
