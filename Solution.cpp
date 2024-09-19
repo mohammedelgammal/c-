fc1b737fda9d015d9426bcb69e5fe23ef0e22f64
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2024-09-19 09:16:51 +0300

    implementing_removeNthFromEnd_problem_solution_tcO(n)_scO(1)

--- Solution.cpp
+++ Solution.cpp
@@ -3,29 +3,17 @@
 
 #include "Solution"
 
-void Solution::reorderList(ListNode *head)
+ListNode *Solution::removeNthFromEnd(ListNode *head, int n)
 {
-    ListNode *slow = head, *fast = head->next;
-    while (fast && fast->next)
-    {
-        slow = slow->next;
-        fast = fast->next->next;
+    ListNode *dummyHead = new ListNode(0, head), *right = head, *left = dummyHead;
+    while(n) {
+        right = right->next;
+        n--;
     }
-    ListNode *prev = nullptr, *curr = slow->next, *next = nullptr;
-    slow->next = nullptr;
-    while (curr)
-    {
-        next = curr->next;
-        curr->next = prev;
-        prev = curr;
-        curr = next;
+    while(right) {
+        right = right->next;
+        left = left->next;
     }
-    ListNode *first = head, *second = prev, *next1 = nullptr, *next2 = nullptr;
-    while(second) {
-        next1 = first->next;
-        next2 = second->next;
-        first->next = second;
-        second->next = next1;
-        first = next1, second = next2;
-    }
+    left->next = left->next->next;
+    return dummyHead->next;
 }
