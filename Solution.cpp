34beba403ca30e76aea81837fffa4423fc930ab4
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2024-10-02 13:55:18 +0300

    implementing_hasCycle_problem_solution_tcO(n)_scO(1)

--- Solution.cpp
+++ Solution.cpp
@@ -3,21 +3,13 @@
 
 #include "Solution"
 
-ListNode *Solution::addTwoNumbers(ListNode *l1, ListNode *l2)
+bool Solution::hasCycle(ListNode *head)
 {
-    ListNode *ans = new ListNode(), *dummy = ans;
-    int carry = 0;
-    while (l1 || l2 || carry)
-    {
-        int val1 = l1 ? l1->val : 0,
-            val2 = l2 ? l2->val : 0,
-            val = val1 + val2 + carry;
-        carry = val / 10;
-        val %= 10;
-        dummy->next = new ListNode(val);
-        dummy = dummy->next;
-        l1 = l1 ? l1->next : nullptr;
-        l2 = l2 ? l2->next : nullptr;
+    ListNode *slow = head, *fast = head ? head->next : nullptr;
+    while(fast && fast->next) {
+        if(slow == fast) return true;
+        slow = slow->next;
+        fast = fast->next->next;
     }
-    return ans->next;
+    return false;
 }
