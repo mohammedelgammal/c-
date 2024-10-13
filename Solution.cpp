e2bfc47012cb16be852edd97c27365423f135183
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2024-10-14 01:48:03 +0300

    implementing_MyQueue_problem_solution_tcO(N)_scO(N)

--- Solution.cpp
+++ Solution.cpp
@@ -3,14 +3,3 @@
 
 #include "Solution"
 
-bool Solution::containsNearbyDuplicate(vector<int> &nums, int k)
-{
-    set<int> set;
-    int l = 0, r = 0;
-    while(r < nums.size()) {
-        if(r - l > k) set.erase(nums[l++]);
-        if(set.contains(nums[r])) return true;
-        set.insert(nums[r++]);
-    }
-    return false;
-}
