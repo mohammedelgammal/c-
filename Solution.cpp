6127a3041b609c8dfb688444490b28642c17781b
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2024-10-13 18:29:39 +0300

    implementing_reverseString_problem_solution_tcO(N)_scO(1)

--- Solution.cpp
+++ Solution.cpp
@@ -3,13 +3,12 @@
 
 #include "Solution"
 
-string Solution::mergeAlternately(string word1, string word2)
+void Solution::reverseString(vector<char> &s)
 {
-    string ans;
-    int l = 0, r = 0, n1 = word1.size(), n2 = word2.size();
-    while(l < n1 || r < n2) {
-        if(l < n1) ans += word1[l++];
-        if(r < n2) ans += word2[r++];
+    int l = 0, r = s.size() - 1;
+    while(r > l) {
+        char tmp = s[r];
+        s[r--] = s[l];
+        s[l++] = tmp;
     }
-    return ans;
 }
