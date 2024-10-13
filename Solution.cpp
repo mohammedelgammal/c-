55309256dd3fc7c0b1b7d668a7a8fabe94923f6f
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2024-10-14 01:12:46 +0300

    implementing_containsNearbyDuplicate_problem_solution_tcO(N)_scO(N)

--- Solution.cpp
+++ Solution.cpp
@@ -3,12 +3,14 @@
 
 #include "Solution"
 
-void Solution::reverseString(vector<char> &s)
+bool Solution::containsNearbyDuplicate(vector<int> &nums, int k)
 {
-    int l = 0, r = s.size() - 1;
-    while(r > l) {
-        char tmp = s[r];
-        s[r--] = s[l];
-        s[l++] = tmp;
+    set<int> set;
+    int l = 0, r = 0;
+    while(r < nums.size()) {
+        if(r - l > k) set.erase(nums[l++]);
+        if(set.contains(nums[r])) return true;
+        set.insert(nums[r++]);
     }
+    return false;
 }
