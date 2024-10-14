dd65821352abaeb470a44620c7ab8eb33620090b
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2024-10-14 19:40:06 +0300

    implementing_makeGood_problem_solution_tcO(N)_scO(N)

--- Solution.cpp
+++ Solution.cpp
@@ -3,3 +3,17 @@
 
 #include "Solution"
 
+string Solution::makeGood(string s)
+{
+    stack<char> stack;
+    for(char ch : s) {
+        if(!stack.empty() && abs(ch - stack.top()) == 32) stack.pop();
+        else stack.push(ch);
+    }
+    string ans(stack.size(), ' ');
+    for(int i = stack.size() - 1; i >= 0; i--) {
+        ans[i] = stack.top();
+        stack.pop();
+    }
+    return ans;
+}
