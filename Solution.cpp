f6907f1547d7fc1610068f6c2dbce135cb8b13bd
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2024-10-25 00:15:42 +0300

    implementing_inorderTraversal_problem_solution_tcO(N)_scO(N)_recursively

--- Solution.cpp
+++ Solution.cpp
@@ -3,17 +3,18 @@
 
 #include "Solution"
 
-string Solution::makeGood(string s)
+void traverseIn(TreeNode *root, vector<int> &ans)
 {
-    stack<char> stack;
-    for(char ch : s) {
-        if(!stack.empty() && abs(ch - stack.top()) == 32) stack.pop();
-        else stack.push(ch);
-    }
-    string ans(stack.size(), ' ');
-    for(int i = stack.size() - 1; i >= 0; i--) {
-        ans[i] = stack.top();
-        stack.pop();
-    }
+    if (!root)
+        return;
+    traverseIn(root->left, ans);
+    ans.push_back(root->val);
+    traverseIn(root->right, ans);
+}
+
+vector<int> Solution::inorderTraversal(TreeNode *root)
+{
+    vector<int> ans;
+    traverseIn(root, ans);
     return ans;
 }
