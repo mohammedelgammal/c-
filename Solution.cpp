8fa194ad55aeb81351d9e5c3116f5bdce1abc314
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2024-10-28 01:57:22 +0300

    implementing_invertTree_problem_solution_tcO(N)_scO(N)_recursively

--- Solution.cpp
+++ Solution.cpp
@@ -3,24 +3,18 @@
 
 #include "Solution"
 
-vector<int> Solution::postorderTraversal(TreeNode *root)
+void invert(TreeNode *root)
 {
-    vector<int> ans;
-    stack<pair<TreeNode*, bool>> stack;
-    stack.push({root, false});
-    while(!stack.empty()) {
-        root = stack.top().first;
-        bool isVisited = stack.top().second;
-        stack.pop();
-        if(root) {
-            if(!isVisited) {
-                stack.push({root, true});
-                stack.push({root->right, false});
-                stack.push({root->left, false});
-            } else {
-                ans.push_back(root->val);
-            }
-        }
-    }
-    return ans;
+    if (!root)
+        return;
+    swap(root->left, root->right);
+    invert(root->left);
+    invert(root->right);
+}
+
+TreeNode *Solution::invertTree(TreeNode *root)
+{
+    TreeNode *curr = root;
+    invert(root);
+    return curr;
 }
