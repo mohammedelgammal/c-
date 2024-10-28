3da7a1ddd095fdb178b35391468211c3532d632e
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2024-10-28 22:43:19 +0300

    implementing_diameterOfBinaryTree_problem_solution_tcO(N)_scO(N)_recursively

--- Solution.cpp
+++ Solution.cpp
@@ -3,15 +3,16 @@
 
 #include "Solution"
 
-int getMax(TreeNode *root, int count)
-{
-    if (!root)
-        return count;
-    return max(getMax(root->left, count + 1),
-               getMax(root->right, count + 1));
+int dfs(TreeNode *root, int &ans) {
+    if(!root) return 0;
+    int left = dfs(root->left, ans),
+        right = dfs(root->right, ans);
+    ans = max(left + right, ans);
+    return max(left, right) + 1;
 }
 
-int Solution::maxDepth(TreeNode *root)
-{
-    return getMax(root, 0);
+int Solution::diameterOfBinaryTree(TreeNode *root) {
+    int ans = 0;
+    dfs(root, ans);
+    return ans;
 }
