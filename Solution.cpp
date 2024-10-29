7437c6a6e04f95ced14379e76070dd5089016e56
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2024-10-29 03:36:15 +0300

    implementing_isBalanced_problem_solution_tcO(N)_scO(N)

--- Solution.cpp
+++ Solution.cpp
@@ -3,16 +3,17 @@
 
 #include "Solution"
 
-int dfs(TreeNode *root, int &ans) {
+int dfs(TreeNode *root, bool &ans) {
     if(!root) return 0;
     int left = dfs(root->left, ans),
         right = dfs(root->right, ans);
-    ans = max(left + right, ans);
+    if(abs(left - right) > 1) ans = false;
     return max(left, right) + 1;
 }
 
-int Solution::diameterOfBinaryTree(TreeNode *root) {
-    int ans = 0;
+bool Solution::isBalanced(TreeNode *root)
+{
+    bool ans = true;
     dfs(root, ans);
     return ans;
 }
