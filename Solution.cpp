4a8becfa10c99f5ec4a8ff80d913531f81c4afcb
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2024-11-04 23:59:05 +0200

    implementing_kthLargest_problem_solution_tcO(NlogN, logN)_scO(n)

--- Solution.cpp
+++ Solution.cpp
@@ -3,14 +3,10 @@
 
 #include "Solution"
 
-TreeNode *mergeTrees(TreeNode *root1, TreeNode *root2)
+int Solution::add(int val)
 {
-    if (!root1 && !root2)
-        return nullptr;
-    if (!root1 || !root2)
-        return !root1 ? root2 : root1;
-    root1->val = root1->val + root2->val;
-    root1->left = mergeTrees(root1->left, root2->left);
-    root1->right = mergeTrees(root1->right, root2->right);
-    return root1;
+    pq.push(val);
+    if (pq.size() > k)
+        pq.pop();
+    return pq.top();
 }
--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-    Solution solution;
+    // Solution solution;
 
     // solution.reverseList();
 
