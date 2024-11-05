d8af57076f278f1c5f991c5493b7be1443994c66
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2024-11-05 22:43:15 +0200

    implementing_findKthLargest_problem_solution_tcO(NlogN)_scO(1)_sorting

--- Solution.cpp
+++ Solution.cpp
@@ -3,17 +3,7 @@
 
 #include "Solution"
 
-int Solution::lastStoneWeight(vector<int>& stones) {
-    priority_queue<int> pq;
-    for(int stone : stones) pq.push(stone);
-    while(pq.size() > 1) {
-        int first = pq.top();
-        pq.pop();
-        int second = pq.top();
-        pq.pop();
-        if(first != second) 
-            pq.push(first - second);
-    }
-    pq.push(0);
-    return pq.top();
+int Solution::findKthLargest(vector<int> &nums, int k) {
+    sort(nums.begin(), nums.end());
+    return nums[nums.size() - k];
 }
