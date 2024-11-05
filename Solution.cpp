ab81140385c7c432d19f6a8d1f24961f1f721c5d
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2024-11-05 22:32:09 +0200

    implementing_lastStoneWeight_problem_solution_tcO(NlogN)_scO(N)

--- Solution.cpp
+++ Solution.cpp
@@ -3,10 +3,17 @@
 
 #include "Solution"
 
-int Solution::add(int val)
-{
-    pq.push(val);
-    if (pq.size() > k)
+int Solution::lastStoneWeight(vector<int>& stones) {
+    priority_queue<int> pq;
+    for(int stone : stones) pq.push(stone);
+    while(pq.size() > 1) {
+        int first = pq.top();
         pq.pop();
+        int second = pq.top();
+        pq.pop();
+        if(first != second) 
+            pq.push(first - second);
+    }
+    pq.push(0);
     return pq.top();
 }
