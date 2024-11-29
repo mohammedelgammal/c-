73f7315e4824a32e5b81bf0c70c517f9ec925556
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2024-11-29 08:01:32 +0200

    implementing_numIslands_problem_solution_tcO(m*n)_scO(m*n)_iterative_queue

--- Solution.cpp
+++ Solution.cpp
@@ -3,18 +3,6 @@
 
 #include "Solution"
 
-int explore(int i, int j, set<pair<int, int>> &visited, vector<vector<char>> &grid)
-{
-    if (i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] == '0' || visited.contains({i, j}))
-        return 0;
-    visited.insert({i, j});
-    explore(i + 1, j, visited, grid);
-    explore(i, j + 1, visited, grid);
-    explore(i - 1, j, visited, grid);
-    explore(i, j - 1, visited, grid);
-    return 1;
-}
-
 int Solution::numIslands(vector<vector<char>> &grid)
 {
     int ans = 0;
@@ -23,9 +11,33 @@ int Solution::numIslands(vector<vector<char>> &grid)
     {
         for (int j = 0; j < grid[0].size(); j++)
         {
-            if (!visited.contains({i, j}))
+            if (grid[i][j] == '1' && !visited.contains({i, j}))
             {
-                ans += explore(i, j, visited, grid);
+                queue<pair<int, int>> queue;
+                visited.insert({i, j});
+                queue.push({i, j});
+                while (!queue.empty())
+                {
+                    pair<int, int> curr = queue.front();
+                    queue.pop();
+                    int ic = curr.first, jc = curr.second;
+                    vector<pair<int, int>> neighbors = {
+                        {ic + 1, jc},
+                        {ic, jc + 1},
+                        {ic - 1, jc},
+                        {ic, jc - 1},
+                    };
+                    for (pair<int, int> &neighbor : neighbors)
+                    {
+                        int in = neighbor.first, jn = neighbor.second;
+                        if (in >= 0 && jn >= 0 && in < grid.size() && jn < grid[0].size() && grid[in][jn] == '1' && !visited.contains(neighbor))
+                        {
+                            visited.insert(neighbor);
+                            queue.push(neighbor);
+                        }
+                    }
+                }
+                ans += 1;
             }
         }
     }
