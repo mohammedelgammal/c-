5c7bbf458a4ea3c25a0e1825f4b88842dd4e5ad5
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2024-12-02 11:33:20 +0200

    implementing_countSubIslands_problem_solution_tcO(m*n)_scO(m*n)_recursively

--- Solution.cpp
+++ Solution.cpp
@@ -4,45 +4,30 @@
 #include "Solution"
 
 int Solution::countSubIslands(vector<vector<int>> &grid1,
-                    vector<vector<int>> &grid2)
+                              vector<vector<int>> &grid2)
 {
     int count = 0;
     set<pair<int, int>> visited;
+    function<bool(int, int)> isSub = [&](int i, int j)
+    {
+        if (i < 0 || j < 0 || i >= grid2.size() || j >= grid2[0].size() || !grid2[i][j] || visited.contains({i, j}))
+        {
+            return true;
+        }
+        visited.insert({i, j});
+        bool isCurrSub = grid1[i][j];
+        isCurrSub &= isSub(i + 1, j);
+        isCurrSub &= isSub(i, j + 1);
+        isCurrSub &= isSub(i - 1, j);
+        isCurrSub &= isSub(i, j - 1);
+        return isCurrSub;
+    };
     for (int i = 0; i < grid2.size(); i++)
     {
         for (int j = 0; j < grid2[0].size(); j++)
         {
-            if (grid2[i][j] && !visited.contains({i, j}))
-            {
-                stack<pair<int, int>> stack;
-                bool isSub = grid1[i][j];
-                visited.insert({i, j});
-                stack.push({i, j});
-                while (!stack.empty())
-                {
-                    pair<int, int> curr = stack.top();
-                    stack.pop();
-                    int ic = curr.first,
-                        jc = curr.second;
-                    vector<pair<int, int>> neighbors = {
-                        {ic + 1, jc}, {ic, jc + 1}, {ic - 1, jc}, {ic, jc - 1}};
-                    for (pair<int, int> &neighbor : neighbors)
-                    {
-                        int in = neighbor.first,
-                            jn = neighbor.second;
-                        if (in >= 0 && jn >= 0 && in < grid2.size() && jn < grid2[0].size() && !visited.contains(neighbor) && grid2[in][jn])
-                        {
-                            isSub &= grid1[in][jn];
-                            visited.insert(neighbor);
-                            stack.push(neighbor);
-                        }
-                    }
-                }
-                if (isSub)
-                {
-                    count += 1;
-                }
-            }
+            if (grid2[i][j] && !visited.contains({i, j}) && isSub(i, j))
+                count += 1;
         }
     }
     return count;
