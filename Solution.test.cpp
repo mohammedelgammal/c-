31f85e2c0ba23a970dffc49c00dfa4799d07680e
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2025-01-21 09:08:25 +0200

    implementingmaxSubarraySumCircular_problem_solution_tcO(n)_scO(1)

--- Solution.test.cpp
+++ Solution.test.cpp
@@ -3,6 +3,6 @@
 #include <gtest/gtest.h>
 
 TEST(MaximumDepthOfValidParenthesis, includingOtherCharacters) {
-  string s = "(1+(2*3)+((8)/4))+1";
-  EXPECT_EQ(maxDepth(s), 3);
+  vector<int> nums{1, -2, 3, -2};
+  EXPECT_EQ(maxSubarraySumCircular(nums), 3);
 }
