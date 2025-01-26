c585ab2659509c62af2379913a6983648e446337
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2025-01-26 10:17:22 +0200

    implementing_insert_problem_solution_tcO(n)_scO(n)_ans_space

--- Solution.test.cpp
+++ Solution.test.cpp
@@ -2,7 +2,9 @@
 
 #include <gtest/gtest.h>
 
-TEST(maxTurbulenceSize, onlyIncrementing) {
-  vector<int> nums{4, 8, 12, 16};
-  EXPECT_EQ(maxTurbulenceSize(nums), 2);
+TEST(insertInterval, IntersectionIntervals) {
+  vector<vector<int>> intervals = {{1, 2}, {3, 5}, {6, 7}, {8, 10}, {12, 16}};
+  vector<int> target{4, 8};
+  vector<vector<int>> ans{{1, 2}, {3, 10}, {12, 16}};
+  EXPECT_EQ(insert(intervals, target), ans);
 }
