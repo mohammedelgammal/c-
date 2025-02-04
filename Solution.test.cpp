989bad05a9add2d819ea537179e11adfc734e304
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2025-02-04 21:31:32 +0200

    implementing_eraseOverlapIntervals_problem_solution_tcO(n)_scO(1)

--- Solution.test.cpp
+++ Solution.test.cpp
@@ -3,6 +3,5 @@
 #include <gtest/gtest.h>
 
 TEST(eraseOverlapIntervals, IntersectingIntervals) {
-  vector<vector<int>> intervals = {{1, 2}, {2, 3}, {3, 4}, {1, 3}};
-  EXPECT_EQ(eraseOverlapIntervals(intervals), 1);
+  EXPECT_EQ(convertToTitle(701), "ZY");
 }
